e2750e92d7104df358ad725c3d9f038cdc56ac3d
Rian Finnegan <rian@amphoradata.com>
2019-08-04 15:22:56 +1000

    test in memory store behaviour

--- apps/test-unit/stores/AmphoraStoreTests.cs
+++ apps/test-unit/stores/AmphoraStoreTests.cs
@@ -5,6 +5,7 @@ using Xunit;
 using Amphora.Common.Models;
 using Amphora.Api.Stores;
 using Amphora.Api.Contracts;
+using System.Linq;
 
 namespace Amphora.Tests.Unit
 {
@@ -17,14 +18,52 @@ namespace Amphora.Tests.Unit
             var sut = new InMemoryEntityStore<AmphoraModel>();
             var a = new AmphoraModel()
             {
-                Title= "Test Title",
+                Title = "Test Title",
                 Description = "Test Description",
                 SchemaId = "not a real schema I reckon",
                 Price = 42
             };
 
             StoreAndRetrieveAmphora(sut, a);
-            
+        }
+
+        [Fact]
+        public void InMemoryEntityStoreList()
+        {
+            var sut = new InMemoryEntityStore<AmphoraModel>();
+            var a = new AmphoraModel()
+            {
+                Title = "Test Title",
+                Description = "Test Description",
+                SchemaId = "not a real schema I reckon",
+                Price = 42
+            };
+
+            ListAmphora(sut, a);
+        }
+
+        [Fact]
+        public void InMemoryEntityStoreReturnsNull()
+        {
+            var sut = new InMemoryEntityStore<AmphoraModel>();
+            GetMissingAmphora(sut);
+        }
+
+        private void GetMissingAmphora(IAmphoraEntityStore<AmphoraModel> sut)
+        {
+            var missing = sut.Get("missing");
+            Assert.Null(missing);
+        }
+
+        private void ListAmphora(IAmphoraEntityStore<AmphoraModel> sut, AmphoraModel a)
+        {
+            var emptyList = sut.ListIds();
+            Assert.Equal(0, emptyList.Count());
+            var setResult = sut.Set(a);
+            var listIds = sut.ListIds();
+            Assert.Equal(1, listIds.Count());
+            Assert.NotNull(listIds.FirstOrDefault());
+            Assert.Equal(setResult.Id, listIds.FirstOrDefault());
         }
 
         private void StoreAndRetrieveAmphora(IAmphoraEntityStore<AmphoraModel> sut, AmphoraModel a)
@@ -33,6 +72,11 @@ namespace Amphora.Tests.Unit
             Assert.NotNull(setResult);
             Assert.NotNull(setResult.Id);
 
+            var id = a.Id;
+
+            var getResult = sut.Get(id);
+            Assert.Equal(id, getResult.Id);
+            Assert.Equal(setResult, getResult);
         }
     }
 }
