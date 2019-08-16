d0781d5d2d5e9bc80a10557fd391c3e2a410d29b
Rian Finnegan <rian@amphoradata.com>
2019-08-16 12:12:06 +1000

    authorize the domain page

--- apps/api/Pages/Domains/Index.cshtml.cs
+++ apps/api/Pages/Domains/Index.cshtml.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
 using Amphora.Common.Models.Domains;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Amphora.Api.Pages.Domains
 {
+    [Authorize]
     public class IndexModel: PageModel
     {
         public List<Domain> Domains {get;set;}
