9da7508ae2fb9e2e082f5ea5bccd687732efb751
Rian Finnegan <rian@amphoradata.com>
2019-08-19 11:47:46 +1000

    move to /infra
    use scripts not the task

---
+++ infra/scripts/run-pulumi.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
+set -e -x
+
+# Add the pulumi CLI to the PATH
+export PATH=$PATH:$HOME/.pulumi/bin
+
+pushd infra/
+
+npm install
+npm run build
+
+pulumi stack select acmeCorp/acmeProject/acme-ui
+
+# https://docs.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=vsts
+case $BUILD_REASON in
+  PullRequest)
+      pulumi preview
+    ;;
+  BuildCompletion|BatchedCI)
+      pulumi up --yes
+    ;;
+  *)
+esac
+
+# Save the stack output variables to job variables.
+echo "##vso[task.setvariable variable=kvUri;isOutput=true]$(pulumi stack output kvUri)"
+
+popd
