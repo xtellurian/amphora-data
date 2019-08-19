3b66663aae4aab1cb2ae2603be677624fbd3cb21
Rian Finnegan <rian@amphoradata.com>
2019-08-20 09:19:47 +1000

    add object id separately (seems dumb)

--- infra/components/state/state.ts
+++ infra/components/state/state.ts
@@ -155,6 +155,14 @@ export class State extends pulumi.ComponentResource {
         keyPermissions: ["create", "get"],
         secretPermissions: ["list", "set", "get", "delete"]
       })
+      const spAccess_objectId = new azure.keyvault.AccessPolicy("sp-access", 
+      {
+        keyVaultId: kv.id,
+        objectId: this.azConfig.clientConfig.servicePrincipalObjectId,
+        tenantId: this.azConfig.clientConfig.tenantId,
+        keyPermissions: ["create", "get"],
+        secretPermissions: ["list", "set", "get", "delete"]
+      })
     }
 
     
