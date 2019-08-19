27453e3f6cc390cb88a6fbcddc1381b36b67b33e
Rian Finnegan <rian@amphoradata.com>
2019-08-19 19:49:10 +1000

    remove -x

--- infra/pulumi-up.sh
+++ infra/pulumi-up.sh
@@ -3,7 +3,7 @@
 # this should run in CI
 
 # exit if a command returns a non-zero exit code and also print the commands and their args as they are executed
-set -e -x
+set -e
 
 # Add the pulumi CLI to the PATH
 export PATH=$PATH:$HOME/.pulumi/bin
