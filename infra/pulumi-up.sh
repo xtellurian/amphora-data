5f710a6be45889459abe53453f25a27f16263c3f
Rian Finnegan <rian@amphoradata.com>
2019-08-20 12:13:03 +1000

    set the source branch (for now) as the stack

--- infra/pulumi-up.sh
+++ infra/pulumi-up.sh
@@ -33,11 +33,13 @@ if [ $BUILD_REASON == "PullRequest" ] ; then
   set_special_stack $SYSTEM_PULLREQUEST_TARGETBRANCH
   echo "Previewing special target stack!"
   pulumi preview
+  echo "Source Branch (Pull Request) is $SYSTEM_PULLREQUEST_SOURCEBRANCH"
+  set_special_stack $SYSTEM_PULLREQUEST_SOURCEBRANCH
+else
+  # spin up the source branch stack
+  echo "Source Branch is $BUILD_SOURCEBRANCH"
+  set_special_stack $BUILD_SOURCEBRANCH
 fi
-
-# spin up the source branch stack
-echo "Source Branch is $BUILD_SOURCEBRANCH"
-set_special_stack $BUILD_SOURCEBRANCH
 
 pulumi up --yes
 
