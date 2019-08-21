3a2881cd84433f59ed0c718a31818a9108d1c22b
Rian Finnegan <rian@amphoradata.com>
2019-08-21 18:14:16 +1000

    make deployment better
    show the githash

--- apps/acr-task.sh
+++ apps/acr-task.sh
@@ -4,4 +4,4 @@ az acr task create -n $TASK_NAME -r $ACR_NAME --branch $BRANCH -f apps/acr-task.
     -c https://dev.azure.com/amphoradata/Amphora/_git/Amphora --git-access-token $ACCESS_TOKEN \
     --commit-trigger-enabled false
 
-az acr task run -n $TASK_NAME -r $ACR_NAME
+az acr task run -n $TASK_NAME -r $ACR_NAME --arg "gitHash=$GITHASH"
