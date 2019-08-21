60811a864a0b8bbcd77cbd9f4b93e9d52c866e32
Rian Finnegan <rian@amphoradata.com>
2019-08-21 19:18:48 +1000

    echo the githash

--- apps/acr-task.sh
+++ apps/acr-task.sh
@@ -2,6 +2,8 @@
 
 az acr task create -n $TASK_NAME -r $ACR_NAME --branch $BRANCH -f apps/acr-task.yaml \
     -c https://dev.azure.com/amphoradata/Amphora/_git/Amphora --git-access-token $ACCESS_TOKEN \
-    --commit-trigger-enabled false --arg "gitHash=$GITHASH"
+    --commit-trigger-enabled false 
 
-az acr task run -n $TASK_NAME -r $ACR_NAME
+echo "Githash is $GITHASH"
+
+az acr task run -n $TASK_NAME -r $ACR_NAME --arg "gitHash=$GITHASH"
