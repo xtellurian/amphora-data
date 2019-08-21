0dabe794feb833a45169758f3f5cfa0788e687a2
Rian Finnegan <rian@amphoradata.com>
2019-08-21 18:55:36 +1000

    put the arg on the task create

--- apps/acr-task.sh
+++ apps/acr-task.sh
@@ -2,6 +2,6 @@
 
 az acr task create -n $TASK_NAME -r $ACR_NAME --branch $BRANCH -f apps/acr-task.yaml \
     -c https://dev.azure.com/amphoradata/Amphora/_git/Amphora --git-access-token $ACCESS_TOKEN \
-    --commit-trigger-enabled false
+    --commit-trigger-enabled false --arg "gitHash=$GITHASH"
 
-az acr task run -n $TASK_NAME -r $ACR_NAME --arg "gitHash=$GITHASH"
+az acr task run -n $TASK_NAME -r $ACR_NAME
