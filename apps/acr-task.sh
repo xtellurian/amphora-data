02e7a5fcf5d6bb6a063d2953212e5fcea67db238
Rian Finnegan <rian@amphoradata.com>
2019-08-23 13:56:22 +1000

    to see what happens in this step

--- apps/acr-task.sh
+++ apps/acr-task.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e -x
 
 az acr task create -n $TASK_NAME -r $ACR_NAME --branch $BRANCH -f apps/acr-task.yaml \
     -c https://dev.azure.com/amphoradata/Amphora/_git/Amphora --git-access-token $ACCESS_TOKEN \
