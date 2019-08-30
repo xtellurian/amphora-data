9a9e1d7852d3d41660c4ca9e42b43b81a3703065
Rian Finnegan <rian@amphoradata.com>
2019-08-30 15:57:52 +1000

    move deploy into script
    can be run locally

---
+++ infra/deploy-app.sh
@@ -0,0 +1,32 @@
+#1/bin/bash
+set -e -x
+cd $(dirname "$0")
+
+if [ -z "$ACR_NAME" ]; then
+    ACR_NAME=$(pulumi stack output acrName)
+fi
+if [ -z "$IMAGE" ]; then
+    IMAGE="$ACR_NAME.azurecr.io/webapp"
+fi
+if [ -z "$GITHASH" ]; then
+    GITHASH=$(git rev-parse HEAD)
+fi
+if [ -z "$BUILD" ]; then
+    BUILD=local
+fi
+if [ -z "$WEBAPPID" ]; then
+    WEBAPPID=$(pulumi stack output webAppResourceId)
+fi
+if [ -z "$CONTEXT" ]; then
+    CONTEXT="../apps"
+fi
+
+az acr login -n $ACR_NAME
+docker build -t $IMAGE:latest -t $IMAGE:$GITHASH -t $IMAGE:$BUILD --build-arg gitHash=$GITHASH $CONTEXT
+docker push $IMAGE:latest
+docker push $IMAGE:$GITHASH
+docker push $IMAGE:$BUILD 
+echo "Setting CI for web app"
+WEBHOOKURI=$(az webapp deployment container config --enable-cd true --ids "$WEBAPPID" --output tsv --query CI_CD_URL)
+echo "Webhook is $WEBHOOKURI"
+az acr webhook create -n WebAppCD -r $ACR_NAME --scope webapp:latest --actions push delete --uri $WEBHOOKURI
