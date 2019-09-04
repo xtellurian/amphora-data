9a8045e8a53caf90570eda5b7293cc9fcdc11ef5
Rian Finnegan <rian@amphoradata.com>
2019-09-04 19:14:55 +1000

    add resource permissions to common

--- apps/api/Authorization/Operations.cs
+++ apps/api/Authorization/Operations.cs
@@ -1,3 +1,4 @@
+using Amphora.Common.Models;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 
 namespace Amphora.Api.Authorization
@@ -5,12 +6,12 @@ namespace Amphora.Api.Authorization
     public static class Operations
     {
         public static OperationAuthorizationRequirement Create =
-            new OperationAuthorizationRequirement { Name = nameof(Create) };
+            new OperationAuthorizationRequirement { Name = ResourcePermissions.Create };
         public static OperationAuthorizationRequirement Read =
-            new OperationAuthorizationRequirement { Name = nameof(Read) };
+            new OperationAuthorizationRequirement { Name = ResourcePermissions.Read };
         public static OperationAuthorizationRequirement Update =
-            new OperationAuthorizationRequirement { Name = nameof(Update) };
+            new OperationAuthorizationRequirement { Name = ResourcePermissions.Update };
         public static OperationAuthorizationRequirement Delete =
-            new OperationAuthorizationRequirement { Name = nameof(Delete) };
+            new OperationAuthorizationRequirement { Name = ResourcePermissions.Delete };
     }
 }
---
+++ apps/common/Models/ResourcePermissions.cs
@@ -0,0 +1,10 @@
+namespace Amphora.Common.Models
+{
+    public static class ResourcePermissions
+    {
+        public static string Create => nameof(Create);
+        public static string Read => nameof(Read);
+        public static string Update => nameof(Update);
+        public static string Delete => nameof(Delete);
+    }
+}
