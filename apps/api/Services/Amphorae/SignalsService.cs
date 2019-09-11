67fa2d4d048edf2271fef242fa000165b55955a4
Rian Finnegan <rian@amphoradata.com>
2019-09-11 10:58:28 +1000

    refactor models into directories

---
+++ apps/api/Services/Amphorae/SignalsService.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+
+namespace Amphora.Api.Services.Amphorae
+{
+    public class SignalsService
+    {
+        public void PushSignal()
+        {
+            // TODO
+        }
+    }
+}
