9cead75837800c7b73e114c573f2d2020df90bfd
Rian Finnegan <rian@amphoradata.com>
2019-10-09 10:30:08 +1100

    add sendgrid CName record

--- central/central.ts
+++ central/central.ts
@@ -222,3 +222,39 @@ const verifyCName = new azure.dns.CNameRecord("verifySquarespace",
     },
     opts,
 );
+
+// Send Grid
+
+const sendGridCName1 = new azure.dns.CNameRecord("sendGridCName1",
+    {
+        name: "em2252",
+        record: "u12987374.wl125.sendgrid.net",
+        resourceGroupName: rg.name,
+        tags,
+        ttl: 60,
+        zoneName: dnsZone.name,
+    },
+    opts,
+);
+const sendGridCName2 = new azure.dns.CNameRecord("sendGridCName2",
+    {
+        name: "s1._domainkey",
+        record: "s1.domainkey.u12987374.wl125.sendgrid.net",
+        resourceGroupName: rg.name,
+        tags,
+        ttl: 60,
+        zoneName: dnsZone.name,
+    },
+    opts,
+);
+const sendGridCName3 = new azure.dns.CNameRecord("sendGridCName3",
+    {
+        name: "s2._domainkey",
+        record: "s2.domainkey.u12987374.wl125.sendgrid.net",
+        resourceGroupName: rg.name,
+        tags,
+        ttl: 60,
+        zoneName: dnsZone.name,
+    },
+    opts,
+);
