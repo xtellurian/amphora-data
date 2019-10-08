95788bfd370b213dea8513d223f0ec3f4d82434d
Rian Finnegan <rian@amphoradata.com>
2019-10-09 10:29:58 +1100

    remove BCC for an auto bcc a feature in SendGrid

--- apps/api/Options/SendGridOptions.cs
+++ apps/api/Options/SendGridOptions.cs
@@ -3,7 +3,6 @@ namespace Amphora.Api.Options
     public class SendGridOptions
     {
         public string ApiKey { get; set; }
-        public string BccEmail { get; set; }
         public string FromEmail { get; set; }
         public string FromName { get; set; }
 
