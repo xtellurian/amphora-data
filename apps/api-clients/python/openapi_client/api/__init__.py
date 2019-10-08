4b32a5c89ed5253cd952ccc2193c0bd409e16048
Rian Finnegan <rian@amphoradata.com>
2019-10-08 13:24:55 +1100

    move client generation to github

--- apps/api-clients/python/openapi_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from openapi_client.api.amphora_api import AmphoraApi
-from openapi_client.api.amphora_schema_api import AmphoraSchemaApi
