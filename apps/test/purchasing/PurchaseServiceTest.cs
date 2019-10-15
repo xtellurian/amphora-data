900429fceeca0bae36d91aa33d560c0fca315a5f
Rian Finnegan <rian@amphoradata.com>
2019-10-16 10:08:13 +1100

    consider emails confirmed on unit test

--- apps/test/purchasing/PurchaseServiceTest.cs
+++ apps/test/purchasing/PurchaseServiceTest.cs
@@ -16,10 +16,17 @@ namespace Amphora.Tests.Unit.Purchasing
         {
             var context = base.GetContext();
             var store = new PurchaseEFStore(context);
-            var user = new ApplicationUser()
+            var user_emailconfirmed = new ApplicationUser()
             {
-                Email = "test@amphoradata.com"
+                Email = "test@amphoradata.com",
+                EmailConfirmed = true
             };
+            var user_notconfirmed = new ApplicationUser()
+            {
+                Email = "other@amphoradata.com",
+                EmailConfirmed = false
+            };
+
             var amphora = new AmphoraModel();
             amphora.Id = System.Guid.NewGuid().ToString();
             var mockEmailSender = new Mock<IEmailSender>();
@@ -29,11 +36,15 @@ namespace Amphora.Tests.Unit.Purchasing
 
             var sut = new PurchaseService(store, null, mockEmailSender.Object, logger);
 
-            var result = await sut.PurchaseAmphora(user, amphora);
+            var result = await sut.PurchaseAmphora(user_emailconfirmed, amphora);
             Assert.True(result.Succeeded);
             Assert.NotNull(result.Entity);
+            var result_notconfirmed = await sut.PurchaseAmphora(user_notconfirmed, amphora);
+            Assert.True(result_notconfirmed.Succeeded);
+            Assert.NotNull(result_notconfirmed.Entity);
 
-            mockEmailSender.Verify(mock => mock.SendEmailAsync(It.Is<string>(s => s == user.Email), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+            mockEmailSender.Verify(mock => mock.SendEmailAsync(It.Is<string>(s => s == user_emailconfirmed.Email), It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+            mockEmailSender.Verify(mock => mock.SendEmailAsync(It.Is<string>(s => s == user_notconfirmed.Email), It.IsAny<string>(), It.IsAny<string>()), Times.Never());
         }
     }
 }
