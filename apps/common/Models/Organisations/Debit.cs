9c6ba265463f37498b3e479f3250e709567e6fb8
Rian Finnegan <rian@amphoradata.com>
2019-10-24 11:11:41 +1100

    #492 add balance to user interface

---
+++ apps/common/Models/Organisations/Debit.cs
@@ -0,0 +1,14 @@
+namespace Amphora.Common.Models.Organisations
+{
+    public class Debit
+    {
+        public Debit() {}
+        public Debit(string label, double amount)
+        {
+            Label = label;
+            Amount = amount;
+        }
+        public double? Amount { get; set; }
+        public string Label { get; set; }
+    }
+}
