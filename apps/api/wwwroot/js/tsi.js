47051cd4c649ec3c2b5bd612c972b4ed00d815e8
Rian Finnegan <rian@amphoradata.com>
2019-10-28 16:13:48 +1100

    render availability chart #510

--- apps/api/wwwroot/js/tsi.js
+++ apps/api/wwwroot/js/tsi.js
@@ -1,22 +1,87 @@
 
 async function tsi(id, signals) {
 
-    var scheme = new ColorScheme;
+
     var tsiClient = new TsiClient();
     var startDate = new Date();
     startDate.setDate(startDate.getDate() - 30);
     endDate = new Date();
     endDate.setDate(endDate.getDate() + 7);
-    var linechartTsqExpressions = [];
 
+    var linechartTsqExpressions = await getLineChartExpressions(tsiClient, signals, startDate, endDate);
+
+
+    const url = window.location.host + "/api"
+    console.log(url)
+    var result = await tsiClient.server.getTsqResults("token", url, linechartTsqExpressions.map(function (ae) {
+        return ae.toTsq()
+    }))
+    var transformedResult = tsiClient.ux.transformTsqResultsForVisualization(result, linechartTsqExpressions);
+
+    var lineChart = new tsiClient.ux.LineChart(document.getElementById('chart'));
+    var availability = new tsiClient.ux.AvailabilityChart(document.getElementById('availability'));
+
+    var brushMoveEndAction = async (from, to, timezone) => {
+        var linechartTsqExpressions = await getLineChartExpressions(tsiClient, signals, from, to);
+
+        const url = window.location.host + "/api"
+        var result = await tsiClient.server.getTsqResults("token", url, linechartTsqExpressions.map(function (ae) {
+            return ae.toTsq()
+        }))
+        var transformedResult = tsiClient.ux.transformTsqResultsForVisualization(result, linechartTsqExpressions);
+
+        var today = (new Date()).toISOString();
+        var statesOrEvents = [
+            {"Component States" : // This key is shown in the legend
+                [
+                    {
+                        [today] : {
+                            'color': '#D869CB',
+                            'description' : 'Now'
+                        }
+                    }
+                ]
+            }
+        ];
+
+        //transformedResult.push(statesOrEvents)
+        // linechartTsqExpressions.push({dataType: 'events', height: 50, eventElementType: 'teardrop'},)
+        lineChart.render(transformedResult,
+            {
+                theme: 'light', grid: true, tooltip: true, legend: 'compact', yAxisState: 'stacked',
+                noAnimate: true, includeDots: true, offset: timezone,
+                includeEnvelope: true, dateLocale: 'en-AU', //events: statesOrEvents
+            },
+            linechartTsqExpressions);
+
+    }
+
+    availability.render([{ availabilityCount: { "": {} } }],
+        { legend: 'hidden', theme: 'light', color: 'red', brushMoveEndAction: brushMoveEndAction, offset: 'Local', isCompact: true },
+        {
+            range:
+            {
+                from: startDate.toISOString(),
+                to: endDate.toISOString()
+            },
+            intervalSize: '1h'
+        }
+    );
+
+    // render empty
+    lineChart.render(transformedResult,
+        { theme: 'light', grid: true, tooltip: true, legend: 'compact', yAxisState: 'stacked', offset: 'Local' });
+}
+
+async function getLineChartExpressions(tsiClient, signals, from, to) {
+    var scheme = new ColorScheme;
     scheme.from_hue(signals.length)
         .scheme('triade')
         .variation('soft');
-
-
     var colors = scheme.colors();
     let count = 0;
 
+    var linechartTsqExpressions = [];
     signals.forEach((sig) => {
         const y = {};
         y[sig.Property] = {
@@ -28,24 +93,11 @@ async function tsi(id, signals) {
         const x = new tsiClient.ux.TsqExpression(
             { timeSeriesId: [id] },
             y,
-            { from: startDate, to: endDate, bucketSize: '6h' },
+            { from, to, bucketSize: '6h' },
             '#' + colors[count++], // color
             sig.Property);
 
         linechartTsqExpressions.push(x);
     });
-
-    const url = window.location.host + "/api"
-    console.log(url)
-    var result = await tsiClient.server.getTsqResults("token", url, linechartTsqExpressions.map(function (ae) { 
-        return ae.toTsq() 
-    }))
-    var transformedResult = tsiClient.ux.transformTsqResultsForVisualization(result, linechartTsqExpressions);
-
-    console.log("linechartTsqExpressions");
-    console.log(linechartTsqExpressions);
-
-    var lineChart = new tsiClient.ux.LineChart(document.getElementById('chart'));
-    const offset = new Date().getTimezoneOffset() * -1; // unsure why TSI needs this flipped...
-    lineChart.render(transformedResult, { theme: 'light', grid: true, tooltip: true, legend: 'compact', yAxisState: 'stacked', offset }, linechartTsqExpressions);
+    return linechartTsqExpressions;
 }
