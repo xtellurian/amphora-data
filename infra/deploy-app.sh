e5201135801a6178c843fecd4f080414c3c724c3
Rian Finnegan <rian@amphoradata.com>
2019-10-30 09:02:59 +1100

    #494 cache build image

--- infra/deploy-app.sh
+++ infra/deploy-app.sh
@@ -31,8 +31,14 @@ if [ -z "$CACHED_IMAGE" ]; then
 fi
 
 az acr login -n $ACR_NAME
-docker pull $CACHED_IMAGE || true # don't fail on image doesn't exist.
+docker pull $CACHED_IMAGE || true # don't fail when cached image doesn't exist.
+
+# build
+docker build --target builder --cache-from $CACHED_IMAGE -t $CACHED_IMAGE $CONTEXT # rebuild the cache
 docker build --cache-from $CACHED_IMAGE -t $IMAGE:latest -t $IMAGE:$GITHASH -t $IMAGE:$BUILD --build-arg gitHash=$GITHASH $CONTEXT
+
+# push
+docker push $CACHED_IMAGE
 docker push $IMAGE:latest
 docker push $IMAGE:$GITHASH
 docker push $IMAGE:$BUILD 
