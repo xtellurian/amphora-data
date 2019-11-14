4b1355e8cee284999018186989bfaedf858eb8e4
Rian Finnegan <rian@amphoradata.com>
2019-11-14 16:00:13 +1100

    use forwarded headers for URL generation

--- apps/api/Startup.cs
+++ apps/api/Startup.cs
@@ -148,7 +148,7 @@ namespace Amphora.Api
             {
                 app.UseStatusCodePagesWithRedirects("/Home/StatusCode?code={0}");
                 app.UseExceptionHandler("/Home/Error");
-                
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -166,6 +166,7 @@ namespace Amphora.Api
            }); // serve Swagger UI
             app.UseReDoc(); // serve ReDoc UI
 
+            app.UseForwardedHeaders();
 
             app.UseAuthentication();
             app.UseAuthorization();
