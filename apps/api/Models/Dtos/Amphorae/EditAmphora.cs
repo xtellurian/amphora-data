c52af8c77917dc43181033d51566250636c2d298
Rian Finnegan <rian@amphoradata.com>
2019-11-25 13:40:05 +1100

    #558 #627 feat: can edit terms and conditions

---
+++ apps/api/Models/Dtos/Amphorae/EditAmphora.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Amphora.Api.Models.Dtos.Amphorae
+{
+    public class EditAmphora : AmphoraDto
+    {
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        [Display(Name = "Latitude")]
+        public double? Lat { get; set; }
+        [Display(Name = "Longitude")]
+        public double? Lon { get; set; }
+
+        [Display(Name = "Terms and Conditions")]
+        public string TermsAndConditionsId { get; set; }
+    }
+}
