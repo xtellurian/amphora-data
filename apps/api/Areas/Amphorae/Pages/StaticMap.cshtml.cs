e55a6b37c770310758982b74ae83b3b6c3ba6213
Rian Finnegan <rian@amphoradata.com>
2019-11-29 09:28:57 +1100

    feat: better map resolution

--- apps/api/Areas/Amphorae/Pages/StaticMap.cshtml.cs
+++ apps/api/Areas/Amphorae/Pages/StaticMap.cshtml.cs
@@ -1,9 +1,11 @@
 using System.Threading.Tasks;
 using Amphora.Api.Contracts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Amphora.Api.Areas.Amphorae.Pages
 {
+    [Authorize]
     public class StaticMapModel : AmphoraPageModel
     {
         private readonly IMapService mapService;
@@ -13,12 +15,20 @@ namespace Amphora.Api.Areas.Amphorae.Pages
             this.mapService = mapService;
         }
 
-        public async Task<IActionResult> OnGetAsync(string id)
+        public async Task<IActionResult> OnGetAsync(string id, int? height, int? width)
         {
             await base.LoadAmphoraAsync(id);
             if (Amphora != null && Amphora.GeoLocation != null && Amphora.GeoLocation.Lat().HasValue && Amphora.GeoLocation.Lon().HasValue)
             {
-                var image = await mapService.GetStaticMapImageAsync(Amphora.GeoLocation);
+                byte[] image;
+                if (height.HasValue && width.HasValue)
+                {
+                    image = await mapService.GetStaticMapImageAsync(Amphora.GeoLocation, height.Value, width.Value);
+                }
+                else
+                {
+                    image = await mapService.GetStaticMapImageAsync(Amphora.GeoLocation);
+                }
                 if (image == null) return Redirect("/images/stock/earth.jpg");
                 return File(image, "image/png"); // image is a png
             }
