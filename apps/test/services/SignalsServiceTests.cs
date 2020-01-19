b63fb2c9616860a6b730178b0bcc219aae76fc9f
Rian Finnegan <rian@amphoradata.com>
2020-01-20 10:59:40 +1100

    test: reproduce #887

---
+++ apps/test/services/SignalsServiceTests.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Amphora.Api.Contracts;
+using Amphora.Api.Services.Amphorae;
+using Amphora.Api.Stores.EFCore;
+using Amphora.Common.Contracts;
+using Amphora.Common.Models.Amphorae;
+using Amphora.Common.Models.Permissions;
+using Amphora.Common.Models.Signals;
+using Amphora.Common.Services.Azure;
+using Moq;
+using Xunit;
+
+namespace Amphora.Tests.Unit.Services
+{
+    public class SignalsServiceTests : UnitTestBase
+    {
+        public SignalsServiceTests()
+        { }
+
+        [Fact]
+        public async Task UploadStringToNumericSignal_ReturnsError()
+        {
+            var context = GetContext();
+            var mockSender = new Mock<IEventHubSender>();
+            var mockUserService = new Mock<IUserService>();
+            var mockPermissionService = new Mock<IPermissionService>();
+            mockPermissionService.Setup(_ => _.IsAuthorizedAsync(It.IsAny<IUser>(),
+                                                                 It.IsAny<AmphoraModel>(),
+                                                                 It.IsAny<AccessLevels>())).Returns(Task.FromResult(true));
+            var mockTsi = new Mock<ITsiService>();
+            var signalStore = new SignalsEFStore(context, CreateMockLogger<SignalsEFStore>());
+            var mockPrincipal = new Mock<ClaimsPrincipal>();
+            var sut = new SignalsService(mockSender.Object,
+                                         mockUserService.Object,
+                                         mockPermissionService.Object,
+                                         mockTsi.Object,
+                                         signalStore,
+                                         CreateMockLogger<SignalsService>());
+
+            var signal = new SignalModel("test", SignalModel.Numeric);
+            var amphora = new AmphoraModel();
+            amphora.Signals.Add(new AmphoraSignalModel(amphora, signal));
+            signal = await signalStore.CreateAsync(signal);
+
+            var data = new Dictionary<string, object>()
+            {
+                { "test", "an unexpected string" }
+            };
+            var res = await sut.WriteSignalAsync(mockPrincipal.Object, amphora, data);
+            Assert.False(res.WasForbidden); // make sure this isn't a permissions test
+            Assert.False(res.Succeeded);
+            var data_ok = new Dictionary<string, object>()
+            {
+                { "test", 2 }
+            };
+            var res_ok = await sut.WriteSignalAsync(mockPrincipal.Object, amphora, data_ok);
+            Assert.False(res_ok.WasForbidden); // make sure this isn't a permissions test
+            Assert.True(res_ok.Succeeded);
+        }
+    }
+}
