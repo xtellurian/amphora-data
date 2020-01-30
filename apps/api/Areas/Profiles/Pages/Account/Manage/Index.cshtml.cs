52a23b825036bfb01260b974ad7c815f57eb72b4
Rian Finnegan <rian@amphoradata.com>
2020-01-30 11:25:44 +1100

    feat: authorise profile settings page

--- apps/api/Areas/Profiles/Pages/Account/Manage/Index.cshtml.cs
+++ apps/api/Areas/Profiles/Pages/Account/Manage/Index.cshtml.cs
@@ -1,8 +1,28 @@
+using System.Threading.Tasks;
+using Amphora.Api.Contracts;
+using Amphora.Common.Models.Users;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Amphora.Api.Areas.Profiles.Pages.Account.Manage
 {
+    [Authorize]
     public class IndexModel : PageModel
     {
+        private readonly IUserService userService;
+
+        public IndexModel(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public ApplicationUser AppUser { get; private set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            this.AppUser = await userService.ReadUserModelAsync(User);
+            return Page();
+        }
     }
 }
