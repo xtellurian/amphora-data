888f7cbb2b5e82fe47578f4dd4a208c04a670325
Rian Finnegan <rian@amphoradata.com>
2020-01-31 17:44:41 +1100

    feat: use native pulumi front door

--- central/central.ts
+++ central/central.ts
@@ -1,6 +1,5 @@
 import * as azure from "@pulumi/azure";
 import * as pulumi from "@pulumi/pulumi";
-import { frontDoorArm } from "./front-door-arm";
 
 const authConfig = new pulumi.Config("authentication");
 
@@ -81,26 +80,6 @@ const dnsZone = new azure.dns.Zone("centralDnsZone",
     },
     opts,
 );
-
-const parameters: pulumi.Input<{ [key: string]: any; }> = {
-    frontDoorName: "amphora",
-};
-
-for (let i = 0; i < prodBackendCount; i++) {
-    parameters[`prod${i}`] = prodHostnames.apply((h) => h[i]);
-}
-
-const template = new azure.core.TemplateDeployment("frontDoorArm",
-    {
-        deploymentMode: "Incremental",
-        parameters,
-        resourceGroupName: rg.name,
-        templateBody: JSON.stringify(frontDoorArm({ tags, prodBackendCount })),
-    },
-    {
-        dependsOn: dnsZone,
-        protect: true,
-    });
 
 const wwwCName = new azure.dns.CNameRecord("wwwCName",
     {
@@ -264,3 +243,130 @@ const sendGridCName3 = new azure.dns.CNameRecord("sendGridCName3",
     },
     opts,
 );
+
+const backendPools: pulumi.Input<Array<pulumi.Input<azure.types.input.frontdoor.FrontdoorBackendPool>>> = [];
+// add the squarespace backend
+backendPools.push(
+    {
+        backends: [{
+            address: "azalea-orca-x567.squarespace.com",
+            hostHeader: "azalea-orca-x567.squarespace.com",
+            httpPort: 80,
+            httpsPort: 443,
+        }],
+        healthProbeName: "normal",
+        loadBalancingName: "loadBalancingSettings1",
+        name: "squarespaceBackend",
+    });
+
+const prodBackends: Array<pulumi.Input<azure.types.input.frontdoor.FrontdoorBackendPoolBackend>> = [];
+for (let i = 0; i < prodBackendCount; i++) {
+    prodBackends.push({
+        address: prodHostnames.apply((h) => h[i]),
+        hostHeader: prodHostnames.apply((h) => h[i]),
+        httpPort: 80,
+        httpsPort: 443,
+    });
+}
+
+const prodBackendPool: azure.types.input.frontdoor.FrontdoorBackendPool = {
+    backends: prodBackends,
+    healthProbeName: "normal",
+    loadBalancingName: "loadBalancingSettings1",
+    name: "prodBackend",
+};
+
+backendPools.push(prodBackendPool);
+
+const frontDoor = new azure.frontdoor.Frontdoor("fd", {
+    backendPoolHealthProbes: [{
+        intervalInSeconds: 30,
+        name: "normal",
+        path: "/",
+        protocol: "Https",
+    }],
+    backendPoolLoadBalancings: [{
+        name: "loadBalancingSettings1",
+        sampleSize: 4,
+        successfulSamplesRequired: 2,
+    }],
+    backendPools,
+    enforceBackendPoolsCertificateNameCheck: true,
+    frontendEndpoints: [{
+        customHttpsProvisioningEnabled: false,
+        hostName: "amphora.azurefd.net",
+        name: "defaultFrontend",
+        sessionAffinityEnabled: true,
+    }, {
+        customHttpsConfiguration: {
+            azureKeyVaultCertificateSecretName: "static-site",
+            azureKeyVaultCertificateSecretVersion: "2cb1416e06e640229d2e28bacb1eb9cd",
+            azureKeyVaultCertificateVaultId: kv.id,
+            certificateSource: "AzureKeyVault",
+        },
+        customHttpsProvisioningEnabled: true,
+        hostName: "amphoradata.com",
+        name: "rootDomain",
+        sessionAffinityEnabled: true,
+    }, {
+        customHttpsConfiguration: {
+            certificateSource: "FrontDoor",
+        },
+        customHttpsProvisioningEnabled: true,
+        hostName: "www.amphoradata.com",
+        name: "wwwDomain",
+        sessionAffinityEnabled: true,
+    }, {
+        customHttpsConfiguration: {
+            certificateSource: "FrontDoor",
+        },
+        customHttpsProvisioningEnabled: true,
+        hostName: "beta.amphoradata.com",
+        name: "betaDomain",
+        sessionAffinityEnabled: true,
+    }],
+    location: "global",
+    name: "amphora",
+    resourceGroupName: rg.name,
+    routingRules: [
+        {
+            acceptedProtocols: [
+                "Https",
+            ],
+            forwardingConfiguration: {
+                backendPoolName: "squarespaceBackend",
+                cacheQueryParameterStripDirective: "StripNone",
+                forwardingProtocol: "HttpsOnly",
+            },
+            frontendEndpoints: ["defaultFrontend", "rootDomain", "wwwDomain"],
+            name: "routeToSquarespace",
+            patternsToMatches: ["/*"],
+        },
+        {
+            acceptedProtocols: [
+                "Https",
+            ],
+            forwardingConfiguration: {
+                backendPoolName: "prodBackend",
+                cacheEnabled: false,
+                cacheQueryParameterStripDirective: "StripNone",
+                forwardingProtocol: "HttpsOnly",
+            },
+            frontendEndpoints: ["betaDomain"],
+            name: "routeToProdEnvironment",
+            patternsToMatches: ["/*"],
+        },
+        {
+            acceptedProtocols: [
+                "Http",
+            ],
+            frontendEndpoints: ["defaultFrontend", "rootDomain", "wwwDomain", "betaDomain"],
+            name: "redirectToHttps",
+            patternsToMatches: ["/*"],
+            redirectConfiguration: {
+                redirectProtocol: "HttpsOnly",
+                redirectType: "Found",
+            },
+        },
+    ],
+});
