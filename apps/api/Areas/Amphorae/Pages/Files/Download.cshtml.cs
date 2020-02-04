228a4fa374db40ec93a16ef2b0913bdef8454cd4
Rian Finnegan <rian@amphoradata.com>
2020-02-04 14:02:59 +1100

    remove: unused

--- apps/api/Areas/Amphorae/Pages/Files/Download.cshtml.cs
+++ apps/api/Areas/Amphorae/Pages/Files/Download.cshtml.cs
@@ -1,7 +1,6 @@
 using System.Threading.Tasks;
 using Amphora.Api.Contracts;
 using Amphora.Common.Models.Amphorae;
-using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
