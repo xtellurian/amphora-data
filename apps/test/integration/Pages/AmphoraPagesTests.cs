e375466fdd62b22702c693b74aaa0053d703b811
Rian Finnegan <rian@amphoradata.com>
2020-02-24 09:45:03 +1100

    refactor: theory to fact

--- apps/test/integration/Pages/AmphoraPagesTests.cs
+++ apps/test/integration/Pages/AmphoraPagesTests.cs
@@ -70,21 +70,20 @@ namespace Amphora.Tests.Integration.Pages
             await DestroyUserAsync(otherClient, otherUser);
         }
 
-        [Theory]
-        [InlineData("/Amphorae/StaticMap")]
-        public async Task CanLoadImage(string path)
+        [Fact]
+        public async Task CanLoadImage()
         {
+            var path = "/Amphorae/StaticMap";
             var (adminClient, adminUser, adminOrg) = await NewOrgAuthenticatedClientAsync();
 
-            var dto = Helpers.EntityLibrary.GetAmphoraDto(adminOrg.Id, nameof(CanLoadImage));
-            adminClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            var dto = Helpers.EntityLibrary.GetAmphoraDto(adminOrg.Id);
             var createResponse = await adminClient.PostAsJsonAsync("api/amphorae", dto);
-            createResponse.EnsureSuccessStatusCode();
+            Assert.True(createResponse.IsSuccessStatusCode);
             dto = JsonConvert.DeserializeObject<DetailedAmphora>(await createResponse.Content.ReadAsStringAsync());
             var id = dto.Id;
-
+            await Task.Delay(500); // wait 0.5 seconds
             var response = await adminClient.GetAsync($"{path}?id={id}");
-            response.EnsureSuccessStatusCode();
+            Assert.True(response.IsSuccessStatusCode);
             Assert.Contains("image/", response.Content.Headers.ContentType.ToString());
 
             await DestroyAmphoraAsync(adminClient, id);
