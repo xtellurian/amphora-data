3e7ce19ed924dfb07280a3e57740708c7e5f50e9
Rian Finnegan <rian@amphoradata.com>
2020-02-26 14:12:37 +1100

    feat: create AKS and MSI (test in pipeline)

--- infra/components/application/aks/aks.ts
+++ infra/components/application/aks/aks.ts
@@ -29,7 +29,9 @@ const tags = {
 const cfg = new pulumi.Config();
 const config = new pulumi.Config("aks");
 
-const password = cfg.requireSecret("aksSpPw");
+const appId = config.require("spAppId");
+const password = cfg.requireSecret("aksSpAppPassword");
+
 const sshPublicKey = cfg.requireSecret("sshPublicKey");
 
 const location = CONSTANTS.location.primary;
@@ -40,6 +42,8 @@ const nodeSize = config.get("nodeSize") || "Standard_D2_v2";
 export class Aks extends pulumi.ComponentResource {
     public k8sCluster: azure.containerservice.KubernetesCluster;
     public k8sProvider: k8s.Provider;
+    public webAppIdentity: azure.authorization.UserAssignedIdentity;
+    private kvAccessPolicies: azure.keyvault.AccessPolicy[] = [];
 
     constructor(
         name: string,
@@ -56,54 +60,72 @@ export class Aks extends pulumi.ComponentResource {
         acr: azure.containerservice.Registry,
         logAnalytics: azure.operationalinsights.AnalyticsWorkspace) {
 
-        // Step 2: Create the AD service principal for the k8s cluster.
-        const adApp = new azuread.Application("aks", {}, { parent: this });
-        const adSp = new azuread.ServicePrincipal("aksSp", { applicationId: adApp.applicationId }, { parent: this });
-        const adSpPassword = new azuread.ServicePrincipalPassword("aksSpPassword", {
-            endDate: "2099-01-01T00:00:00Z",
-            servicePrincipalId: adSp.id,
-            value: password,
+        // Step 2: This step creates an AKS cluster.
+        this.k8sCluster = new azure.containerservice.KubernetesCluster("aksCluster", {
+            addonProfile: {
+                omsAgent: {
+                    enabled: true,
+                    logAnalyticsWorkspaceId: logAnalytics.id,
+                },
+            },
+            defaultNodePool: {
+                name: "standard",
+                nodeCount,
+                vmSize: nodeSize,
+            },
+            dnsPrefix: `${pulumi.getStack()}-kube`,
+            linuxProfile: {
+                adminUsername: "aksuser",
+                sshKey: { keyData: sshPublicKey },
+            },
+            location,
+            resourceGroupName: rg.name,
+            servicePrincipal: {
+                clientId: appId,
+                clientSecret: password,
+            },
+            tags,
         }, {
             parent: this,
         });
 
-        // Step 3: This step creates an AKS cluster.
-        // this.k8sCluster = new azure.containerservice.KubernetesCluster("aksCluster", {
-        //     addonProfile: {
-        //         omsAgent: {
-        //             enabled: true,
-        //             logAnalyticsWorkspaceId: logAnalytics.id,
-        //         },
-        //     },
-        //     defaultNodePool: {
-        //         name: "standard",
-        //         nodeCount,
-        //         vmSize: nodeSize,
-        //     },
-        //     dnsPrefix: `${pulumi.getStack()}-kube`,
-        //     linuxProfile: {
-        //         adminUsername: "aksuser",
-        //         sshKey: { keyData: sshPublicKey },
-        //     },
-        //     location,
-        //     resourceGroupName: rg.name,
-        //     servicePrincipal: {
-        //         clientId: adApp.applicationId,
-        //         clientSecret: adSpPassword.value,
-        //     },
-        //     tags,
-        // }, {
-        //     parent: this,
-        // });
-
         // Expose a k8s provider instance using our custom cluster instance.
-        // this.k8sProvider = new k8s.Provider("aksK8s", {
-        //     kubeconfig: this.k8sCluster.kubeConfigRaw,
-        // }, {
-        //     parent: this,
-        // });
+        this.k8sProvider = new k8s.Provider("aksK8s", {
+            kubeconfig: this.k8sCluster.kubeConfigRaw,
+        }, {
+            parent: this,
+        });
+
+        this.webAppIdentity = new azure.authorization.UserAssignedIdentity("webApp", {
+            location: rg.location,
+            resourceGroupName: rg.name,
+            tags,
+        });
+
+        this.addMsiToKeyVault("k8sWebApp", this.params.kv, this.webAppIdentity);
 
         // Export the kubeconfig
         // this.kubeconfig = k8sCluster.kubeConfigRaw
+    }
+
+    private addMsiToKeyVault(
+        name: string,
+        kv: azure.keyvault.KeyVault,
+        identity: azure.authorization.UserAssignedIdentity) {
+        const ap = new azure.keyvault.AccessPolicy(
+            name,
+            {
+                applicationId: identity.clientId,
+                keyPermissions: ["unwrapKey", "wrapKey"],
+                keyVaultId: kv.id,
+                objectId: identity.principalId,
+                secretPermissions: ["get", "list"],
+                tenantId: CONSTANTS.authentication.tenantId,
+            },
+            {
+                parent: this,
+            },
+        );
+        this.kvAccessPolicies.push(ap);
     }
 }
