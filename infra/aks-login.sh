f6005cadcf28bc83486486fd3de3f7b88cd25855
Rian Finnegan <rian@amphoradata.com>
2020-03-03 16:09:57 +1100

    fix: aks login multiple

--- infra/aks-login.sh
+++ infra/aks-login.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-set -x
-name=$(pulumi stack output k8sName)
-group=$(pulumi stack output k8sGroup)
+
+while getopts l: option
+do
+case "${option}"
+in
+l) LOCATION=${OPTARG};; # Public IP address of your ingress controller
+esac
+done
+
+# location could be k8sPrimary or k8sSecondary
+
+if [ -z "$LOCATION" ]; then
+    echo "Missing location -l"
+    exit 1
+fi
+
+JSON=$(pulumi stack output $LOCATION)
+name=$( jq -r  '.name' <<< "$(echo $JSON)" ) 
+group=$( jq -r  '.group' <<< "$(echo $JSON)" ) 
+
+echo "Name: $name, Group: $group"
 az aks get-credentials -n "$name" -g "$group"
