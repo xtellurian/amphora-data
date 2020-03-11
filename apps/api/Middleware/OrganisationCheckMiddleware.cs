d57ae81216e3e53da696f74e41752bcc919f2f8f
Rian Finnegan <rian@amphoradata.com>
2020-03-12 10:37:24 +1100

    fix: after create org, delay in update cookie

--- apps/api/Middleware/OrganisationCheckMiddleware.cs
+++ apps/api/Middleware/OrganisationCheckMiddleware.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Amphora.Api.Contracts;
 using Amphora.Common.Contracts;
 using Amphora.Common.Extensions;
+using Amphora.Common.Models.Users;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 
@@ -15,7 +15,8 @@ namespace Amphora.Api.Middleware
         private readonly RequestDelegate _next;
         private readonly ILogger<OrganisationCheckMiddleware> logger;
 
-        public OrganisationCheckMiddleware(RequestDelegate next, ILogger<OrganisationCheckMiddleware> logger)
+        public OrganisationCheckMiddleware(RequestDelegate next,
+                                           ILogger<OrganisationCheckMiddleware> logger)
         {
             _next = next;
             this.logger = logger;
@@ -40,6 +41,18 @@ namespace Amphora.Api.Middleware
         {
             var organisationId = httpContext.User.GetOrganisationId();
             var name = httpContext.User.GetUserName();
+            var userId = httpContext.User.GetUserId();
+
+            if (string.IsNullOrEmpty(organisationId))
+            {
+                // organisation ID may exist
+                var userDetails = await userService.UserStore.ReadAsync(userId);
+                if (!string.IsNullOrEmpty(userDetails.OrganisationId))
+                {
+                    organisationId = userDetails.OrganisationId;
+                }
+            }
+
             if (httpContext.User.Identity?.IsAuthenticated == true // don't redirect if there's no user
                  && string.IsNullOrEmpty(organisationId) // only redirect if user doesn't have an org
                  && !AcceptablePaths.Any(_ => string.Equals(httpContext.Request.Path, _, comparisonType: StringComparison.OrdinalIgnoreCase))
