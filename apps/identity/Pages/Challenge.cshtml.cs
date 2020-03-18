a7546fc35db51f8bf863300f230fe3f386e6aae3
Rian Finnegan <rian@amphoradata.com>
2020-03-19 10:27:00 +1100

    feat: redirect to index when auth'd

--- apps/identity/Pages/Challenge.cshtml.cs
+++ apps/identity/Pages/Challenge.cshtml.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Amphora.Identity.Pages
 {
     [Authorize]
     public class ChallengePageModel : PageModelBase
-    { }
+    {
+        public IActionResult OnGet()
+        {
+            return RedirectToPage("./Index");
+        }
+    }
 }
