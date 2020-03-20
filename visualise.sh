0a4865ff77bc95ad6fe5a72f66031000e0b46e8c
Rian Finnegan <rian@amphoradata.com>
2020-03-20 11:01:33 +1100

    fix: script had weird extra bit

--- visualise.sh
+++ visualise.sh
@@ -1,4 +1,4 @@
-docker run --rm -p 8080:80 --name envisaged \                                          develop
+docker run --rm -p 8080:80 --name envisaged \
        -v $(pwd):/visualization/git_repo:ro \
        -e GOURCE_TITLE="Amphora" \
        utensils/envisaged
