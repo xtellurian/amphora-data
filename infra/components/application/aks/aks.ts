997a92b7caf70231c0671f98d78db305b913ad2e
Rian Finnegan <rian@amphoradata.com>
2020-03-24 14:26:25 +1100

    lint: missing semicolon

--- infra/components/application/aks/aks.ts
+++ infra/components/application/aks/aks.ts
@@ -130,7 +130,7 @@ export class Aks extends pulumi.ComponentResource {
         // this.kubeconfig = k8sCluster.kubeConfigRaw
 
         // Step 3: Install dependencies into the cluster
-        const appSettings = getK8sAppSettings(kv, this.k8sCluster.location)
+        const appSettings = getK8sAppSettings(kv, this.k8sCluster.location);
         this.k8sInfra = new K8sInfrastructure(`${this.name}-infra`, {
             appSettings,
             identities: this.identities,
