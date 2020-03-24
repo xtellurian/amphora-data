4a0793459feec166f4d3b3953d3ec772265fa036
Rian Finnegan <rian@amphoradata.com>
2020-03-25 10:34:11 +1100

    feat: add register url that redirects to register

---
+++ apps/api/Pages/Register.cshtml.cs
@@ -0,0 +1,33 @@
+using Amphora.Common.Extensions;
+using Amphora.Common.Models.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Amphora.Api.Pages
+{
+    public class RegisterPageModel : PageModelBase
+    {
+        private readonly IOptionsMonitor<ExternalServices> externalServices;
+
+        public RegisterPageModel(IOptionsMonitor<ExternalServices> externalServices)
+        {
+            this.externalServices = externalServices;
+        }
+
+        public IActionResult OnGet()
+        {
+            var protocol = Request.IsHttps ? "https" : "http";
+            var returnUrl = $"{protocol}://{Request.Host}/Challenge";
+            string registerUrl = $"{externalServices.CurrentValue.IdentityUri().ToStandardString()}/Account/Register?returnUrl={returnUrl}";
+
+            if (!User.Identity.IsAuthenticated)
+            {
+                return Redirect(registerUrl);
+            }
+            else
+            {
+                return RedirectToPage("./Index");
+            }
+        }
+    }
+}
