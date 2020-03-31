41c0c8f3d2be989e483c40ee5b43985e92e947f5
Rian Finnegan <rian@amphoradata.com>
2020-03-31 16:10:01 +1100

    refactor: change names and paths for test

--- apps/test/integration/Pages/OrganisationPagesTests.cs
+++ apps/test/integration/Pages/OrganisationPagesTests.cs
@@ -12,19 +12,38 @@ namespace Amphora.Tests.Integration.Pages
         }
 
         [Theory]
-        [InlineData("/Organisations/Create")]
-        [InlineData("/Organisations/Detail")]
-        [InlineData("/Organisations/Edit")]
-        [InlineData("/Organisations/Index")]
-        [InlineData("/Organisations?Name=kittens")]
-        // [InlineData("/Organisations/Join")] // TODO: needs an invitation
-        [InlineData("/Organisations/Members")]
-        [InlineData("/Organisations/TermsAndConditions")]
-        [InlineData("/Organisations/TermsAndConditions/Create")]
-        // [InlineData("/Organisations/SetRole")] // doesn't work, needs extra params
-        [InlineData("/Organisations/TermsAndConditions/Index")]
-        public async Task CanLoadPage(string path)
+        [InlineData("")]
+        [InlineData("?Name=kittens")]
+        [InlineData("Create")]
+        [InlineData("Detail")]
+        [InlineData("Edit")]
+        [InlineData("TermsAndConditions")]
+        [InlineData("TermsAndConditions/Create")]
+        public async Task CanLoadOrganisationsPage(string pageName)
         {
+            var path = $"/Organisatons/{pageName}";
+            var (adminClient, adminUser, adminOrg) = await NewOrgAuthenticatedClientAsync();
+            var qString = $"?id={adminOrg.Id}";
+            var response = await adminClient.GetAsync(path + qString);
+            await AssertHttpSuccess(response);
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+
+            await DestroyOrganisationAsync(adminClient, adminOrg);
+            await DestroyUserAsync(adminClient, adminUser);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Credits")]
+        [InlineData("Debits")]
+        [InlineData("Invite")]
+        [InlineData("Invoices")]
+        [InlineData("Members")]
+        [InlineData("Plan")]
+        [InlineData("SelectPlan")]
+        public async Task CanLoadAccountPage(string pageName)
+        {
+            var path = $"/Organisations/Account/{pageName}";
             var (adminClient, adminUser, adminOrg) = await NewOrgAuthenticatedClientAsync();
             var qString = $"?id={adminOrg.Id}";
             var response = await adminClient.GetAsync(path + qString);
