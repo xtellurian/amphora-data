b63313eac82690715a5b9429268250da762f6335
Rian Finnegan <rian@amphoradata.com>
2020-04-02 11:27:21 +1100

    lint

--- apps/api/Contracts/IAmphoraFileService.cs
+++ apps/api/Contracts/IAmphoraFileService.cs
@@ -1,7 +1,5 @@
-using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Amphora.Api.Models;
 using Amphora.Api.Models.Dtos.Amphorae.Files;
 using Amphora.Common.Models;
 using Amphora.Common.Models.Amphorae;
@@ -11,7 +9,6 @@ namespace Amphora.Api.Contracts
     public interface IAmphoraFileService
     {
         IBlobStore<AmphoraModel> Store { get; }
-
         Task<EntityOperationResult<UploadResponse>> CreateFileAsync(ClaimsPrincipal principal, AmphoraModel entity, string file);
         Task<EntityOperationResult<byte[]>> ReadFileAsync(ClaimsPrincipal principal, AmphoraModel entity, string file);
         Task<EntityOperationResult<UploadResponse>> WriteFileAsync(ClaimsPrincipal principal, AmphoraModel entity, byte[] contents, string file);
