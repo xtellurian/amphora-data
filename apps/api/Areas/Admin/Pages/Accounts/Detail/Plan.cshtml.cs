0e50513b98e7713a461391ac204ca1303146ddb8
Rian Finnegan <rian@amphoradata.com>
2020-04-07 10:38:58 +1000

    feat: #1287 admin can set plan

--- apps/api/Areas/Admin/Pages/Accounts/Detail/Plan.cshtml.cs
+++ apps/api/Areas/Admin/Pages/Accounts/Detail/Plan.cshtml.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Amphora.Common.Contracts;
 using Amphora.Common.Models.Organisations;
+using Amphora.Common.Models.Organisations.Accounts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Amphora.Api.Areas.Admin.Pages.Accounts.Detail
@@ -15,6 +16,54 @@ namespace Amphora.Api.Areas.Admin.Pages.Accounts.Detail
         {
             if (await LoadOrganisationAsync(id))
             {
+                return Page();
+            }
+            else
+            {
+                return BadRequest(Error);
+            }
+        }
+
+        public async Task<IActionResult> OnPostFreeAsync(string id)
+        {
+            if (await LoadOrganisationAsync(id))
+            {
+                this.Org.Account ??= new Account();
+                this.Org.Account.Plan ??= new Plan();
+                this.Org.Account.Plan.PlanType = Plan.PlanTypes.Free;
+                Org = await orgStore.UpdateAsync(Org);
+                return Page();
+            }
+            else
+            {
+                return BadRequest(Error);
+            }
+        }
+
+        public async Task<IActionResult> OnPostTeamAsync(string id)
+        {
+            if (await LoadOrganisationAsync(id))
+            {
+                this.Org.Account ??= new Account();
+                this.Org.Account.Plan ??= new Plan();
+                this.Org.Account.Plan.PlanType = Plan.PlanTypes.Team;
+                Org = await orgStore.UpdateAsync(Org);
+                return Page();
+            }
+            else
+            {
+                return BadRequest(Error);
+            }
+        }
+
+        public async Task<IActionResult> OnPostInstitutionAsync(string id)
+        {
+            if (await LoadOrganisationAsync(id))
+            {
+                this.Org.Account ??= new Account();
+                this.Org.Account.Plan ??= new Plan();
+                this.Org.Account.Plan.PlanType = Plan.PlanTypes.Institution;
+                Org = await orgStore.UpdateAsync(Org);
                 return Page();
             }
             else
