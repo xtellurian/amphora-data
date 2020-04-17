ab4adbf641f00e52b71d6d1118f7debee4f34f28
Rian Finnegan <rian@amphoradata.com>
2020-04-17 11:22:16 +1000

    refactor: add base class for amphora index page

---
+++ apps/api/Areas/Amphorae/Pages/AmphoraeIndexPageModel.cs
@@ -0,0 +1,77 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amphora.Api.Contracts;
+using Amphora.Api.Pages;
+using Amphora.Common.Contracts;
+using Amphora.Common.Models;
+using Amphora.Common.Models.Amphorae;
+using Amphora.Common.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Amphora.Api.Areas.Amphorae.Pages
+{
+    public abstract class AmphoraeIndexPageModel : PageModelBase
+    {
+
+        protected const int DefaultTop = 8;
+        protected readonly IAmphoraeService amphoraeService;
+        private readonly IUserDataService userDataService;
+
+        public AmphoraeIndexPageModel(IAmphoraeService amphoraeService, IUserDataService userDataService)
+        {
+            this.amphoraeService = amphoraeService;
+            this.userDataService = userDataService;
+        }
+
+        public IEnumerable<AmphoraModel> Amphorae { get; set; } = new List<AmphoraModel>();
+        public int? Count { get; set; } = null;
+        public int? Skip { get; set; } = 0;
+        public int? Top { get; set; } = DefaultTop;
+
+        public int TotalSkip => (Skip ?? 0) * (Top ?? DefaultTop);
+        protected Error Error { get; set; } = null;
+
+        protected async Task<IActionResult> MyAmphorae()
+        {
+            var userReadRes = await userDataService.ReadAsync(User);
+            if (!userReadRes.Succeeded)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Profiles" });
+            }
+
+            // get my amphora
+            var amphorae = amphoraeService.AmphoraStore.Query(a => a.CreatedById == userReadRes.Entity.Id);
+            Count = await amphorae.CountAsync();
+            this.Amphorae = await amphorae.Skip(TotalSkip).Take(Top ?? 0).ToListAsync();
+            return Page();
+        }
+
+        protected async Task<IActionResult> MyPurchasedAmphorae()
+        {
+            var userReadRes = await userDataService.ReadAsync(User);
+            if (!userReadRes.Succeeded) { return RedirectToPage("/Account/Login", new { area = "Profiles" }); }
+
+            var amphorae = await amphoraeService.AmphoraPurchasedBy(User, userReadRes.Entity);
+            Count = await amphorae.CountAsync();
+            this.Amphorae = await amphorae.Skip(TotalSkip).Take(Top ?? 0).ToListAsync();
+            return Page();
+        }
+
+        protected async Task<IActionResult> OrgAmphorae()
+        {
+            var userReadRes = await userDataService.ReadAsync(User);
+            if (!userReadRes.Succeeded)
+            {
+                return RedirectToPage("/Account/Login", new { area = "Profiles" });
+            }
+
+            // get my amphora
+            var amphorae = amphoraeService.AmphoraStore.Query(a => a.OrganisationId == userReadRes.Entity.OrganisationId);
+            Count = await amphorae.CountAsync();
+            this.Amphorae = await amphorae.Skip(TotalSkip).Take(Top ?? 0).ToListAsync();
+            return Page();
+        }
+    }
+}
--- apps/api/Areas/Amphorae/Pages/Index.cshtml.cs
+++ apps/api/Areas/Amphorae/Pages/Index.cshtml.cs
@@ -14,35 +14,25 @@ using Newtonsoft.Json;
 namespace Amphora.Api.Areas.Amphorae.Pages
 {
     [CommonAuthorize]
-    public class IndexModel : PageModel
+    public class IndexPageModel : AmphoraeIndexPageModel
     {
         private readonly IUserDataService userDataService;
         private readonly ISearchService searchService;
-        private readonly IAmphoraeService amphoraeService;
         private readonly IMapper mapper;
 
-        public IndexModel(
+        public IndexPageModel(
             IUserDataService userDataService,
             ISearchService searchService,
             IAmphoraeService amphoraeService,
             IEntityStore<AmphoraModel> entityStore,
-            IMapper mapper)
+            IMapper mapper) : base(amphoraeService, userDataService)
         {
             this.userDataService = userDataService;
             this.searchService = searchService;
-            this.amphoraeService = amphoraeService;
             this.mapper = mapper;
-            this.Amphorae = new List<AmphoraModel>();
         }
 
-        private const int DefaultTop = 8;
-        public IEnumerable<AmphoraModel> Amphorae { get; set; }
         public string Handler { get; private set; }
-        public int? Count { get; set; } = null;
-        public int? Skip { get; set; } = 0;
-        public int? Top { get; set; } = DefaultTop;
-
-        public int TotalSkip => (Skip ?? 0) * (Top ?? DefaultTop);
         [BindProperty]
         public string JsonSelectedAmphoraIdArray { get; set; }
 
@@ -95,46 +85,5 @@ namespace Amphora.Api.Areas.Amphorae.Pages
             // now delete many
             return RedirectToPage("./DeleteMany", new { ids = ids });
         }
-
-        private async Task<IActionResult> MyPurchasedAmphorae()
-        {
-            var userReadRes = await userDataService.ReadAsync(User);
-            if (!userReadRes.Succeeded) { return RedirectToPage("/Account/Login", new { area = "Profiles" }); }
-
-            var amphorae = await amphoraeService.AmphoraPurchasedBy(User, userReadRes.Entity);
-            Count = await amphorae.CountAsync();
-            this.Amphorae = await amphorae.Skip(TotalSkip).Take(Top ?? 0).ToListAsync();
-            return Page();
         }
-
-        private async Task<IActionResult> OrgAmphorae()
-        {
-            var userReadRes = await userDataService.ReadAsync(User);
-            if (!userReadRes.Succeeded)
-            {
-                return RedirectToPage("/Account/Login", new { area = "Profiles" });
-            }
-
-            // get my amphora
-            var amphorae = amphoraeService.AmphoraStore.Query(a => a.OrganisationId == userReadRes.Entity.OrganisationId);
-            Count = await amphorae.CountAsync();
-            this.Amphorae = await amphorae.Skip(TotalSkip).Take(Top ?? 0).ToListAsync();
-            return Page();
-        }
-
-        private async Task<IActionResult> MyAmphorae()
-        {
-            var userReadRes = await userDataService.ReadAsync(User);
-            if (!userReadRes.Succeeded)
-            {
-                return RedirectToPage("/Account/Login", new { area = "Profiles" });
-            }
-
-            // get my amphora
-            var amphorae = amphoraeService.AmphoraStore.Query(a => a.CreatedById == userReadRes.Entity.Id);
-            Count = await amphorae.CountAsync();
-            this.Amphorae = await amphorae.Skip(TotalSkip).Take(Top ?? 0).ToListAsync();
-            return Page();
-        }
-    }
 }
