99b6cfd476b70ed40f9f2cfa24a508cd51b029ae
Rian Finnegan <rian@amphoradata.com>
2020-04-21 09:50:30 +1000

    feat: allow set staging

--- apps/common/Models/Platform/EnvironmentInfo.cs
+++ apps/common/Models/Platform/EnvironmentInfo.cs
@@ -2,6 +2,7 @@ namespace Amphora.Common.Models.Platform
 {
     public class EnvironmentInfo
     {
+        public bool IsStaging { get; set; }
         public bool IsDevelopment { get; set; }
         public string? Stack { get; set; }
         public string? Location { get; set; }
