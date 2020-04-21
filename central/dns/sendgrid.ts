09cddda5cbac995afee8314e960d7dacd581a17d
Rian Finnegan <rian@amphoradata.com>
2020-04-21 12:23:34 +1000

    dev: update send grid dns  for mkt

--- central/dns/sendgrid.ts
+++ central/dns/sendgrid.ts
@@ -13,8 +13,7 @@ export function sendGridDns(rg: azure.core.ResourceGroup, dnsZone: azure.dns.Zon
         source: "pulumi",
         stack: pulumi.getStack(),
     };
-    // Send Grid
-
+    // Send Grid (APP)
     const sendGridCName1 = new azure.dns.CNameRecord("sendGridCName1",
         {
             name: "em2252",
@@ -47,5 +46,38 @@ export function sendGridDns(rg: azure.core.ResourceGroup, dnsZone: azure.dns.Zon
             zoneName: dnsZone.name,
         },
         opts,
+    );
+
+    // Send Grid (Marketting)
+    // Adding for Isaac's marketting campaign tool
+    const markettingCname1 = new azure.dns.CNameRecord("marketing1",
+        {
+            name: "em7374",
+            record: "u15779133.wl134.sendgrid.net",
+            resourceGroupName: rg.name,
+            tags,
+            ttl: 60,
+            zoneName: dnsZone.name,
+        },
+    );
+    const markettingCname2 = new azure.dns.CNameRecord("marketing2",
+        {
+            name: "mkt._domainkey",
+            record: "mkt.domainkey.u15779133.wl134.sendgrid.net",
+            resourceGroupName: rg.name,
+            tags,
+            ttl: 60,
+            zoneName: dnsZone.name,
+        },
+    );
+    const markettingCname3 = new azure.dns.CNameRecord("marketing3",
+        {
+            name: "mkt2._domainkey",
+            record: "mkt2.domainkey.u15779133.wl134.sendgrid.net",
+            resourceGroupName: rg.name,
+            tags,
+            ttl: 60,
+            zoneName: dnsZone.name,
+        },
     );
 }
