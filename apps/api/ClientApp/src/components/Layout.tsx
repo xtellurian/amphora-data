097befa1c2d6e640e3ebdf952753573f41a13baa
Rian Finnegan <rian@amphoradata.com>
2020-04-30 16:21:44 +1000

    wip: disable menu when not logged in

--- apps/api/ClientApp/src/components/Layout.tsx
+++ apps/api/ClientApp/src/components/Layout.tsx
@@ -1,48 +1,59 @@
 import * as React from 'react';
 import { User } from 'oidc-client';
 import { Dispatch } from 'redux';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import { Container } from 'reactstrap';
 import NavMenu from './navigation/NavMenu';
 import HamburgerMenu from './navigation/HamburgerMenu';
 import { ApplicationState } from '../redux/state';
 
 interface LayoutProps {
-    user: User;
-    isLoadingUser: boolean;
-    dispatch: Dispatch;
-    location: any;
-    children?: React.ReactNode
+  user: User;
+  isLoadingUser: boolean;
+  dispatch: Dispatch;
+  location: any;
+  children?: React.ReactNode
 }
 
 const Layout = (props: LayoutProps) => {
-    const isConnected: boolean = !!props.user;
+  const isConnected: boolean = !!props.user;
+  if (isConnected) {
     return (
-        <React.Fragment>
-            <HamburgerMenu isConnected={isConnected} path={props.location.pathname} />
-            <NavMenu isConnected={isConnected} path={props.location.pathname} />
-            <Container>
-                {props.children}
-            </Container>
-        </React.Fragment>
+      <React.Fragment>
+        <HamburgerMenu isConnected={isConnected} path={props.location.pathname} />
+        <NavMenu isConnected={isConnected} path={props.location.pathname} />
+        <Container>
+          {props.children}
+        </Container>
+      </React.Fragment>
     );
+  } else {
+    return (
+      <React.Fragment>
+        <NavMenu isConnected={isConnected} path={props.location.pathname} />
+        <Container>
+          {props.children}
+        </Container>
+      </React.Fragment>
+    )
+  }
 }
 
 function mapStateToProps(state: ApplicationState) {
-    return {
-      user: state.oidc.user,
-      isLoadingUser: state.oidc.isLoadingUser,
-      location: state.router.location,
-    };
-  }
-  
-  function mapDispatchToProps(dispatch: Dispatch) {
-    return {
-      dispatch,
-    };
-  }
-  
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(Layout as any);
+  return {
+    user: state.oidc.user,
+    isLoadingUser: state.oidc.isLoadingUser,
+    location: state.router.location,
+  };
+}
+
+function mapDispatchToProps(dispatch: Dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Layout as any);
