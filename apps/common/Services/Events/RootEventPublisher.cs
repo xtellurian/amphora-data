599096e627c7121223a154dc672c8ffed2f6ed13
Rian Finnegan <rian@amphoradata.com>
2020-05-01 16:59:31 +1000

    dev: log stuff in the root publisher

--- apps/common/Services/Events/RootEventPublisher.cs
+++ apps/common/Services/Events/RootEventPublisher.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Amphora.Common.Contracts;
+using Microsoft.Extensions.Logging;
 
 namespace Amphora.Common.Services.Events
 {
@@ -8,19 +10,44 @@ namespace Amphora.Common.Services.Events
     {
         private readonly ITelemetry telemetryClient;
         private readonly IEventPublisher eventPublisher;
+        private readonly ILogger<RootEventPublisher> logger;
 
-        public RootEventPublisher(ITelemetry telemetryClient, IEventPublisher eventPublisher)
+        public RootEventPublisher(ITelemetry telemetryClient,
+                                  IEventPublisher eventPublisher,
+                                  ILogger<RootEventPublisher> logger)
         {
             this.telemetryClient = telemetryClient;
             this.eventPublisher = eventPublisher;
+            this.logger = logger;
         }
 
         public async Task PublishEventAsync(params IEvent[] events)
         {
             var tasks = new List<Task>();
-            tasks.Add(eventPublisher.PublishEventAsync(events));
-            tasks.Add(telemetryClient.PublishEventAsync(events));
-            await Task.WhenAll(tasks);
+            try
+            {
+                var publisherTask = eventPublisher.PublishEventAsync(events);
+                var telemetryTask = telemetryClient.PublishEventAsync(events);
+                tasks.Add(publisherTask);
+                tasks.Add(telemetryTask);
+
+                // wait for all tasks to complete
+                await Task.WhenAll(tasks);
+
+                if (!publisherTask.IsCompletedSuccessfully)
+                {
+                    logger.LogError("publisherTask did not complete successfully", publisherTask.Exception);
+                }
+
+                if (!telemetryTask.IsCompletedSuccessfully)
+                {
+                    logger.LogError("TelemetryTask did not complete successfully", telemetryTask.Exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError($"Publisher threw exception", ex);
+            }
         }
     }
 }
