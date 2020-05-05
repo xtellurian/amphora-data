b10f0e9953c6bf4f1e0c4c2662e4629ad39e5ae8
Rian Finnegan <rian@amphoradata.com>
2020-05-05 15:00:06 +1000

    refactor: move namespace

--- apps/test/integration/Organisations/TermsOfUseCRUDTests.cs
+++ apps/test/integration/Organisations/TermsOfUseCRUDTests.cs
@@ -2,13 +2,12 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Amphora.Api;
-using Amphora.Api.Models.Dtos;
 using Amphora.Api.Models.Dtos.Organisations;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Newtonsoft.Json;
 using Xunit;
 
-namespace Amphora.Tests.Integration.Organisations
+namespace Amphora.Tests.Integration.Amphorae
 {
     [Collection(nameof(ApiFixtureCollection))]
     public class TermsOfUseCRUDTests : WebAppIntegrationTestBase
@@ -33,13 +32,13 @@ namespace Amphora.Tests.Integration.Organisations
             await AssertHttpSuccess(result);
 
             var dto = JsonConvert.DeserializeObject<TermsOfUse>(contents);
-            Assert.Equal(tou.Id, dto.Id);
+            Assert.NotNull(dto.Id);
+            Assert.Equal(tou.Name, dto.Name);
             Assert.Equal(tou.Contents, dto.Contents);
 
             var response = await adminClient.GetAsync($"api/TermsOfUse");
-            var contents2 = await response.Content.ReadAsStringAsync();
             await AssertHttpSuccess(response);
-            var allTnc = JsonConvert.DeserializeObject<List<TermsOfUse>>(contents2);
+            var allTnc = JsonConvert.DeserializeObject<List<TermsOfUse>>(await response.Content.ReadAsStringAsync());
             Assert.Single(allTnc);
             Assert.Equal(dto.Id, allTnc[0].Id);
             Assert.Equal(dto.Contents, allTnc[0].Contents);
