076b4f96caf5eea487bcb6cab033f1382f0acf71
Rian Finnegan <rian@amphoradata.com>
2020-05-07 14:37:44 +1000

    feat: fail faster

--- apps/infrastructure/Extensions/PollyExtensions.cs
+++ apps/infrastructure/Extensions/PollyExtensions.cs
@@ -9,24 +9,27 @@ namespace Amphora.Infrastructure.Extensions
     {
         public static void AddAzureMapsHttpClient(this IServiceCollection services)
         {
-             services.AddHttpClient(HttpClientNames.AzureMaps)
-            .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
-            {
+            services.AddHttpClient(HttpClientNames.AzureMaps)
+           .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+           {
                 TimeSpan.FromSeconds(1),
                 TimeSpan.FromSeconds(5),
                 TimeSpan.FromSeconds(10)
-            }));
+           }));
         }
 
         public static void AddIdentityServerHttpClient(this IServiceCollection services)
         {
-             services.AddHttpClient(HttpClientNames.IdentityServerClient)
-            .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+            services.AddHttpClient(HttpClientNames.IdentityServerClient, client =>
             {
+                client.Timeout = TimeSpan.FromSeconds(15); // fail faster
+            })
+           .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
+           {
                 TimeSpan.FromSeconds(1),
                 TimeSpan.FromSeconds(5),
                 TimeSpan.FromSeconds(10)
-            }));
+           }));
         }
     }
 }
