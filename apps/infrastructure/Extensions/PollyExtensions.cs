8e92ae87692acfd3987f00d00d13ac286d0d6e2f
Rian Finnegan <rian@amphoradata.com>
2020-05-08 14:13:32 +1000

    dev: explicit policy

--- apps/infrastructure/Extensions/PollyExtensions.cs
+++ apps/infrastructure/Extensions/PollyExtensions.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Net.Http;
 using Amphora.Infrastructure.Models;
 using Microsoft.Extensions.DependencyInjection;
 using Polly;
+using Polly.Extensions.Http;
 
 namespace Amphora.Infrastructure.Extensions
 {
@@ -20,16 +22,21 @@ namespace Amphora.Infrastructure.Extensions
 
         public static void AddIdentityServerHttpClient(this IServiceCollection services)
         {
+            var policy = HttpPolicyExtensions
+                .HandleTransientHttpError() // HttpRequestException, 5XX and 408
+                .OrResult(response => (int)response.StatusCode == 429) // RetryAfter
+                .WaitAndRetryAsync(new[]
+                {
+                    TimeSpan.FromSeconds(2),
+                    TimeSpan.FromSeconds(5),
+                    TimeSpan.FromSeconds(10)
+                });
+
             services.AddHttpClient(HttpClientNames.IdentityServerClient, client =>
             {
                 client.Timeout = TimeSpan.FromSeconds(30); // fail faster
             })
-           .AddTransientHttpErrorPolicy(builder => builder.WaitAndRetryAsync(new[]
-           {
-                TimeSpan.FromSeconds(1),
-                TimeSpan.FromSeconds(5),
-                TimeSpan.FromSeconds(10)
-           }));
+           .AddPolicyHandler(policy);
         }
     }
 }
