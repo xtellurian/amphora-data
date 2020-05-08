8dcf041004c958995aeda3873d7d0a6ce7a945ba
Rian Finnegan <rian@amphoradata.com>
2020-05-08 12:06:58 +1000

    fix: semicolon was ending line input

--- apps/test-ci.sh
+++ apps/test-ci.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [ -z "$CoverletOutput" ]
+then
+    CoverletOutput="$(pwd)"
+fi
+
+echo Coverlet Output Directory is $CoverletOutput
+commonArgs="-l:trx;LogFileName=$CoverletOutput/TestOutput.xml /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$CoverletOutput"
+set -x
 # run the first phase tests
-dotnet test --filter Phase=One -l:trx;LogFileName=/output/TestOutput.xml /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=/output/
+dotnet test --filter Phase=One $commonArgs
 # run all the tests
-dotnet test -l:trx;LogFileName=/output/TestOutput.xml /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=/output/
+dotnet test $commonArgs
