6efdeca0736764320c6a784624c9363eec442d08
Rian Finnegan <rian@amphoradata.com>
2020-05-11 09:35:29 +1000

    fix: missing coverage output?

--- apps/test-ci.sh
+++ apps/test-ci.sh
@@ -6,7 +6,7 @@ then
 fi
 
 echo Coverlet Output Directory is $CoverletOutput
-commonArgs="-l:trx;LogFileName=$CoverletOutput/TestOutput.xml /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$CoverletOutput"
+commonArgs="-l:trx;LogFileName=$CoverletOutput/TestOutput.xml /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=$CoverletOutput/"
 set -x
 # run the first phase tests
 dotnet test --filter Phase=One $commonArgs
