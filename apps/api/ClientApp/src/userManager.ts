81c2c973bd64dcd7d59416aa8e6a32b18097209c
Rian Finnegan <rian@amphoradata.com>
2020-05-14 16:21:27 +1000

    feat: get refresh token

--- apps/api/ClientApp/src/userManager.ts
+++ apps/api/ClientApp/src/userManager.ts
@@ -6,7 +6,7 @@ const userManagerConfig: UserManagerSettings = {
   client_id: 'spa',
   redirect_uri: 'https://localhost:5001/#/callback',
   response_type: 'token id_token',
-  scope:"openid profile web_api",
+  scope:"openid profile web_api offline_access",
   authority: 'http://localhost:6500',
   silent_redirect_uri: 'http://localhost:5100/silentRenew.html',
   automaticSilentRenew: true,
