49e339d905ba4cd127e4ac83b05c4c2203db4401
Rian Finnegan <rian@amphoradata.com>
2020-05-18 10:38:57 +1000

    dev: try to load different authority based on host

--- apps/api/ClientApp/src/userManager.ts
+++ apps/api/ClientApp/src/userManager.ts
@@ -2,19 +2,36 @@
 import { createUserManager } from 'redux-oidc';
 import { UserManagerSettings } from 'oidc-client';
 
-const userManagerConfig: UserManagerSettings = {
-  client_id: 'spa',
-  redirect_uri: 'https://localhost:5001/#/callback',
-  response_type: 'token id_token',
-  scope:"openid profile web_api offline_access",
+
+const settings = {
   authority: 'http://localhost:6500',
-  silent_redirect_uri: 'http://localhost:5100/silentRenew.html',
+  redirect_uri: 'https://localhost:5001/#/callback',
+  silent_redirect_uri: 'http://localhost:5001/silentRenew.html',
+  client_id: 'spa',
+  response_type: 'token id_token',
+  scope: "openid profile web_api offline_access",
   automaticSilentRenew: true,
   filterProtocolClaims: true,
   loadUserInfo: true,
   monitorSession: true
 };
 
-const userManager = createUserManager(userManagerConfig);
+
+function getSettings(): UserManagerSettings {
+
+  const host = window.location.host;
+  const protocol = window.location.protocol;
+
+  if (window.location.host.includes("develop")) {
+    settings.authority = "https://develop.identity.amphoradata.com";
+    settings.redirect_uri = `${protocol}://${host}/#/callback`
+    settings.silent_redirect_uri = `${protocol}://${host}/silentRenew.html`
+  }
+
+  console.log(settings)
+  return settings;
+}
+
+const userManager = createUserManager(getSettings());
 
 export default userManager;
