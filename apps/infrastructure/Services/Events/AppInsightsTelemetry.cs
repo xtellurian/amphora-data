b69805d27d66b4acddb08884f73153c7f4a15ac3
Rian Finnegan <rian@amphoradata.com>
2020-05-18 15:15:34 +1000

    dev: get better insight into events

--- apps/infrastructure/Services/Events/AppInsightsTelemetry.cs
+++ apps/infrastructure/Services/Events/AppInsightsTelemetry.cs
@@ -1,16 +1,20 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Amphora.Common.Contracts;
 using Microsoft.ApplicationInsights;
+using Microsoft.Extensions.Logging;
 
 namespace Amphora.Infrastructure.Services.Events
 {
     public class AppInsightsTelemetry : ITelemetry
     {
         private readonly TelemetryClient client;
+        private readonly ILogger<AppInsightsTelemetry> logger;
 
-        public AppInsightsTelemetry(TelemetryClient client)
+        public AppInsightsTelemetry(TelemetryClient client, ILogger<AppInsightsTelemetry> logger)
         {
             this.client = client;
+            this.logger = logger;
         }
 
         public Task PublishEventAsync(params IEvent[] events)
@@ -20,12 +24,14 @@ namespace Amphora.Infrastructure.Services.Events
                 client.TrackEvent(e.EventType, e.Data);
             }
 
+            logger.LogInformation($"Tracked {events.Count()} Events");
             return Task.CompletedTask;
         }
 
         public void TrackMetricValue(string metric, double? value)
         {
             client.GetMetric(metric).TrackValue(value);
+            logger.LogInformation($"Tracked metric | {metric} = {value}");
         }
     }
 }
