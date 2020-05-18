9d6ef4253170757ad56bd3feb152519804920f56
Rian Finnegan <rian@amphoradata.com>
2020-05-18 10:25:35 +1000

    dev: make app a class

--- apps/api/ClientApp/src/App.tsx
+++ apps/api/ClientApp/src/App.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
 import Layout from './components/Layout';
 import Routes from './Routes'
 
 import './custom.css'
 
-export default () => (
-    <Layout >
-        <Routes />
-    </Layout>
-);
+class App extends React.PureComponent {
+
+    render() {
+        return (
+            <Layout >
+                <Routes />
+            </Layout>
+        );
+    }
+}
+
+export default connect()(App);
+
