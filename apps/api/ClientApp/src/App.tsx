2cd1bacba8d8b799e181c8be3c8a60eb9fe05ec1
Rian Finnegan <rian@amphoradata.com>
2020-05-20 11:32:45 +1000

    dev: global styles for react

--- apps/api/ClientApp/src/App.tsx
+++ apps/api/ClientApp/src/App.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import Layout from './components/Layout';
 import Routes from './Routes'
+import './components/core/fontawesome'; // Load FontAwesome library
 
+// Load all global css
 import './custom.css';
 import './components/core/core.css';
 
