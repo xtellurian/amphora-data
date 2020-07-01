73b30fb746fd2a2886bb11b59beae5e83c1adbaf
Rian Finnegan <rian@amphoradata.com>
2020-07-01 11:07:36 +1000

    dev: add pencil to library

--- apps/api/ClientApp/src/components/core/fontawesome.ts
+++ apps/api/ClientApp/src/components/core/fontawesome.ts
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas, faInfo, faExclamationTriangle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
-import { faTimesCircle, faCheck, faArrowRight, faArrowLeft, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimesCircle, faCheck, faArrowRight, faArrowLeft, faTimes, faPencilAlt } from '@fortawesome/free-solid-svg-icons';
 
-library.add(fas, faTimesCircle, faCheck, faInfo, faExclamationTriangle, faExclamationCircle, faArrowRight, faArrowLeft, faTimes  )
+library.add(fas, faTimesCircle, faCheck, faInfo, faExclamationTriangle, faExclamationCircle, faArrowRight, faArrowLeft, faTimes, faPencilAlt  )
