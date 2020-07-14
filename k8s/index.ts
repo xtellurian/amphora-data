bfb1db8eb02e97d7ef4b9fccb21f596e3000e921
Rian Finnegan <rian@amphoradata.com>
2020-07-14 12:56:51 +1000

    dev: deployments depend on certs

--- k8s/index.ts
+++ k8s/index.ts
@@ -11,7 +11,7 @@ import { Certificates } from "./certificates";
 const stack = pulumi.getStack();
 const stackSplit = stack.split("-");
 if (stackSplit.length != 2) {
-    throw new Error("Stack Name does not conform");
+  throw new Error("Stack Name does not conform");
 }
 
 const environment = stack.split("-")[0];
@@ -20,36 +20,47 @@ const region = stack.split("-")[1];
 const infra = new pulumi.StackReference(`xtellurian/amphora/${environment}`);
 
 const k8sOutputs = infra.requireOutput("k8s");
-const k8sInfo = k8sOutputs.apply(_ => _[region]);
+const k8sInfo = k8sOutputs.apply((_) => _[region]);
 
 const provider = new k8s.Provider("provider1", {
-    kubeconfig: k8sInfo.apply(k => k.kubeConfig),
+  kubeconfig: k8sInfo.apply((k) => k.kubeConfig),
 });
 
 const certs = new Certificates("certificates", {
-    provider,
+  provider,
 });
 
-const frontEnd = new FrontEnd("frontend1", {
+const frontEnd = new FrontEnd(
+  "frontend1",
+  {
     environment,
-    location: <pulumi.Output<string>>k8sInfo.apply(k => k.location),
+    location: <pulumi.Output<string>>k8sInfo.apply((k) => k.location),
     provider,
-});
+  },
+  { dependsOn: certs }
+);
 
-const api = new AmphoraApi("api", {
+const api = new AmphoraApi(
+  "api",
+  {
     environment,
-    location: <pulumi.Output<string>>k8sInfo.apply(k => k.location),
+    location: <pulumi.Output<string>>k8sInfo.apply((k) => k.location),
     provider,
-});
+  },
+  { dependsOn: certs }
+);
 
-const identity = new Identity("identity1", {
+const identity = new Identity(
+  "identity1",
+  {
     environment,
-    location: <pulumi.Output<string>>k8sInfo.apply(k => k.location),
+    location: <pulumi.Output<string>>k8sInfo.apply((k) => k.location),
     provider,
-});
-
+  },
+  { dependsOn: certs }
+);
 
 export let aks = {
-    name: k8sInfo.apply(k => k.name),
-    group: k8sInfo.apply(k => k.group)
-}
+  name: k8sInfo.apply((k) => k.name),
+  group: k8sInfo.apply((k) => k.group),
+};
