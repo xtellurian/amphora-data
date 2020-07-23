9eb478d1620f2b217c2489bda7c12676e7021960
Rian Finnegan <rian@amphoradata.com>
2020-07-23 17:34:37 +1000

    feat: #1731 [v2] download files

--- apps/api/ClientApp/src/components/amphorae/detail/Files.tsx
+++ apps/api/ClientApp/src/components/amphorae/detail/Files.tsx
@@ -1,19 +1,39 @@
 import * as React from "react";
+import { AxiosInstance } from "axios";
 import { useAmphoraClients } from "react-amphora";
 import { OneAmphora } from "./props";
 import { EmptyState } from "../../molecules/empty/EmptyState";
 import * as axios from "axios";
 import { PrimaryButton } from "../../molecules/buttons";
 import { LoadingState } from "../../molecules/empty/LoadingState";
+import { Table } from "../../molecules/tables/Table";
 import { Header } from "./Header";
 import { UploadResponse, DetailedAmphora } from "amphoradata";
-import { AxiosInstance } from "axios";
+import { useHistory } from "react-router";
+import Modal, { Styles } from "react-modal";
+import { Link } from "react-router-dom";
 
 interface FilesState {
     isLoading: boolean;
+    downloadFileName?: string | null;
     fileNames: string[];
     newFileName?: string;
 }
+const modalStyle: Styles = {
+    overlay: {
+        zIndex: 3000, // this should be on top of the hamburger menu.
+    },
+    content: {
+        minWidth: "20rem",
+        top: "50%",
+        left: "50%",
+        right: "auto",
+        bottom: "auto",
+        marginRight: "-50%",
+        transform: "translate(-50%, -50%)",
+    },
+};
+const columns = [{ key: "name", name: "File Name" }];
 
 // backup upload url
 const backupUploadUrl = (id: string, name: string) =>
@@ -24,7 +44,8 @@ function completeUpload(
     axios: AxiosInstance,
     name: string,
     file: File,
-    res?: UploadResponse | null | undefined
+    res?: UploadResponse | null | undefined,
+    callback?: (error?: any | null | undefined) => void
 ) {
     const data = new FormData();
 
@@ -35,38 +56,40 @@ function completeUpload(
     if (res && res.url) {
         axios
             .put(res.url, data)
-            .then((k) => console.log("uploaded file"))
-            .catch((e) => console.log("ERROR Uploading file"));
+            .then((k) => callback && callback())
+            .catch((e) => callback && callback(e));
     } else if (amphora.id) {
         // try backup
         axios
             .put(backupUploadUrl(amphora.id, name), data)
-            .then((k) => console.log("uploaded file backup"))
-            .catch((e) => console.log("uploaded file backup error"));
+            .then((k) => callback && callback())
+            .catch((e) => callback && callback(e));
     } else {
-        console.log("uh oh");
+        callback && callback(true);
     }
 }
 
 const hiddenInputId = "select-file-input";
 
 export const FilesPage: React.FunctionComponent<OneAmphora> = (props) => {
+    const history = useHistory();
     const clients = useAmphoraClients();
     const [state, setState] = React.useState<FilesState>({
         fileNames: [],
         isLoading: false,
+        downloadFileName: null,
     });
 
     const cancelToken = axios.default.CancelToken;
     const source = cancelToken.source();
 
-    React.useEffect(() => {
+    const tryLoadFiles = () => {
         if (props.amphora.id && !state.isLoading) {
             setState({ isLoading: true, fileNames: state.fileNames });
             clients.amphoraeApi
                 .amphoraeFilesListFiles(
                     props.amphora.id,
-                    64,
+                    25,
                     0,
                     "Alphabetical",
                     undefined,
@@ -77,26 +100,25 @@ export const FilesPage: React.FunctionComponent<OneAmphora> = (props) => {
                 )
                 .then((r) => setState({ fileNames: r.data, isLoading: false }))
                 .catch((e) => setState({ fileNames: [], isLoading: false }));
-
-            return () => source.cancel("The files component unmounted");
         }
-    }, [props.amphora, clients, state.isLoading, state.fileNames, source]);
+    };
+
+    React.useEffect(() => {
+        tryLoadFiles();
+        return () => source.cancel("The files component unmounted");
+    }, []);
 
     const triggerUpload = () => {
         const x = document.getElementById(hiddenInputId);
         if (x) {
             x.click();
-        } else {
-            console.log("no xxx");
         }
     };
 
     const onFileChangedHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log("running handler");
         const file = e.target.files && e.target.files[0];
         const localFileName = file && file.name;
         const fileName = state.newFileName || localFileName;
-        console.log(clients.axios);
         if (
             props.amphora.id &&
             file &&
@@ -112,7 +134,8 @@ export const FilesPage: React.FunctionComponent<OneAmphora> = (props) => {
                         clients.axios,
                         fileName,
                         file,
-                        r.data
+                        r.data,
+                        (e) => !e && tryLoadFiles()
                     )
                 )
                 .catch((e) => console.log(e));
@@ -123,12 +146,44 @@ export const FilesPage: React.FunctionComponent<OneAmphora> = (props) => {
         return <LoadingState />;
     }
 
-    if (state.fileNames.length === 0) {
-        return <EmptyState> There are no files in this Amphora.</EmptyState>;
-    }
+    const downloadFile = (fileName: string) => {
+        setState({
+            isLoading: state.isLoading,
+            fileNames: state.fileNames,
+            newFileName: state.newFileName,
+            downloadFileName: fileName,
+        });
+    };
 
+    const closeModal = () => {
+        setState({
+            fileNames: state.fileNames,
+            isLoading: state.isLoading,
+            newFileName: state.newFileName,
+            downloadFileName: null,
+        });
+    };
+    const downloadPath = `/api/amphorae/${props.amphora.id}/files/${state.downloadFileName}`;
     return (
         <React.Fragment>
+            <Modal
+                isOpen={!!state.downloadFileName}
+                shouldCloseOnEsc={true}
+                shouldCloseOnOverlayClick={true}
+                // onAfterOpen={onAfterOpen}
+                onRequestClose={(e) => closeModal()}
+                style={modalStyle}
+                contentLabel="Example Modal"
+            >
+                <div className="text-center">
+                    <Link target="_blank" to={downloadPath}>
+                        <PrimaryButton className="w-75" onClick={() => setTimeout(closeModal, 250)}>
+                            {`Download ${state.downloadFileName}`}
+                        </PrimaryButton>
+                    </Link>
+                </div>
+            </Modal>
+
             <Header title="Files">
                 <input
                     id={hiddenInputId}
@@ -142,10 +197,18 @@ export const FilesPage: React.FunctionComponent<OneAmphora> = (props) => {
                     Upload File{" "}
                 </PrimaryButton>
             </Header>
-
-            {state.fileNames.map((f) => (
-                <p key={f}> {f}</p>
-            ))}
+            {state.fileNames.length == 0 ? (
+                <EmptyState> There are no files in this Amphora.</EmptyState>
+            ) : (
+                <Table
+                    columns={columns}
+                    onRowClicked={(r) => downloadFile(r.name as string)}
+                    rowGetter={(i: number) => {
+                        return { name: state.fileNames[i] };
+                    }}
+                    rowCount={Math.min(25, state.fileNames.length)}
+                />
+            )}
         </React.Fragment>
     );
 };
