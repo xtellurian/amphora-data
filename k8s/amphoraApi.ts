bee553031a770863bf98d98c1868f85370fb0e14
Rian Finnegan <rian@amphoradata.com>
2020-08-04 09:41:01 +1000

    dev: rate limit using nginx too

--- k8s/amphoraApi.ts
+++ k8s/amphoraApi.ts
@@ -173,6 +173,8 @@ export class AmphoraApi extends pulumi.ComponentResource {
           annotations: {
             "kubernetes.io/ingress.class": "nginx",
             "cert-manager.io/cluster-issuer": "letsencrypt",
+            "nginx.ingress.kubernetes.io/limit-rps": "5", // rate limits per second per IP address
+            "nginx.ingress.kubernetes.io/limit-rpm": "300", // rate limits per minute per IP address
             "nginx.ingress.kubernetes.io/rewrite-target": " /$1",
             "nginx.ingress.kubernetes.io/proxy-body-size": "0", // disable body size checking
           },
