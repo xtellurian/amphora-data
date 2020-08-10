ba227134c537a662d8041efd9c021c33eae6ce6d
Rian Finnegan <rian@amphoradata.com>
2020-08-10 18:01:48 +1000

    fix: remove unused import

--- apps/api/ClientApp/src/components/Home.tsx
+++ apps/api/ClientApp/src/components/Home.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { NewWelcome } from "./molecules/landing/NewWelcome";
-import { DiagnosticPage } from "./hidden/DiagnosticPage";
 
 export const Home = (): React.ReactElement => (
     <div>
