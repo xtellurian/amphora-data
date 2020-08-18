34441fa97000c3b09225dadfd4f18e4676e385b8
Rian Finnegan <rian@amphoradata.com>
2020-08-18 11:04:35 +1000

    ux: improve tabbing w/ default in query string

--- apps/api/ClientApp/src/components/molecules/tabs/Tabs.tsx
+++ apps/api/ClientApp/src/components/molecules/tabs/Tabs.tsx
@@ -1,28 +1,43 @@
-import * as React from 'react';
-import './tabs.css';
+import * as React from "react";
+import "./tabs.css";
 
-import { Link } from 'react-router-dom';
-import { RouteComponentProps, withRouter } from 'react-router';
-import { activeTab } from './activeTab';
+import { Link } from "react-router-dom";
+import { RouteComponentProps, withRouter } from "react-router";
+import { activeTab } from "./activeTab";
 
 export interface Tab {
     text?: string;
     id: string;
 }
 
-type TabsProps =
-    {
-        name?: string;
-        tabs: Tab[];
-        default: string;
-    }
-    & RouteComponentProps<{ tab: string }>;
-
+type TabsProps = {
+    name?: string;
+    tabs: Tab[];
+    default: string;
+} & RouteComponentProps<{ tab: string }>;
 
 class Tabs extends React.PureComponent<TabsProps> {
+    private getTabName(): string {
+        return this.props.name || "tab";
+    }
 
     private getActiveTabId(): string {
-        return activeTab(this.props.location.search, this.props.name) || this.props.default;
+        const tab = activeTab(this.props.location.search, this.getTabName());
+        const qs =
+            this.props.location.search.length > 0
+                ? `${this.props.location.search}&`
+                : "?";
+        if (!tab) {
+            this.props.history.push({
+                pathname: this.props.location.pathname,
+                search: `${qs}${this.getTabName()}=${
+                    this.props.default
+                }`,
+            });
+            return this.props.default;
+        } else {
+            return tab;
+        }
     }
 
     private isTabActive(t: Tab) {
@@ -30,25 +45,31 @@ class Tabs extends React.PureComponent<TabsProps> {
     }
 
     renderTab(t: Tab) {
-        const commonClassNames = "tab col-3"
-        const className = this.isTabActive(t) ? `${commonClassNames} active txt-bold` : commonClassNames
+        const commonClassNames = "tab col-3";
+        const className = this.isTabActive(t)
+            ? `${commonClassNames} active txt-bold`
+            : commonClassNames;
         return (
-            <div key={t.id} className={className} >
-                <Link to={`${this.props.location.pathname}?${this.props.name || "tab"}=${t.id}`}>
+            <div key={t.id} className={className}>
+                <Link
+                    to={`${this.props.location.pathname}?${this.getTabName()}=${
+                        t.id
+                    }`}
+                >
                     {t.text || t.id}
                 </Link>
             </div>
-        )
+        );
     }
 
     render() {
         return (
             <React.Fragment>
                 <div className="tabs row no-gutters text-sm">
-                    {this.props.tabs.map(t => this.renderTab(t))}
+                    {this.props.tabs.map((t) => this.renderTab(t))}
                 </div>
             </React.Fragment>
-        )
+        );
     }
 }
 
