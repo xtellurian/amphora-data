90ecd28bca8753cd69ebb0bcceaf1298f79ecfe7
Rian Finnegan <rian@amphoradata.com>
2020-08-21 10:32:40 +1000

    lint

--- infra/components/state/state.ts
+++ infra/components/state/state.ts
@@ -26,16 +26,18 @@ export class State extends pulumi.ComponentResource {
   public cosmosDb: azure.cosmosdb.Account;
   public kvAccessPolicies: azure.keyvault.AccessPolicy[] = [];
 
-  constructor(
-    params: IComponentParams,
-    private monitoring: Monitoring,
-  ) {
+  constructor(params: IComponentParams, private monitoring: Monitoring) {
     super("amphora:State", params.name, {}, params.opts);
 
     this.create();
   }
 
-  public storeInVault(name: string, key: string, value: pulumi.Input<string> | string, parent?: pulumi.Resource) {
+  public storeInVault(
+    name: string,
+    key: string,
+    value: pulumi.Input<string> | string,
+    parent?: pulumi.Resource
+  ) {
     return new azure.keyvault.Secret(
       name,
       {
@@ -47,7 +49,7 @@ export class State extends pulumi.ComponentResource {
       {
         dependsOn: this.kvAccessPolicies,
         parent: parent || this.kv,
-      },
+      }
     );
   }
 
@@ -59,7 +61,7 @@ export class State extends pulumi.ComponentResource {
         location: CONSTANTS.location.primary,
         tags,
       },
-      { parent: this },
+      { parent: this }
     );
 
     this.kv = this.keyvault(stateRg);
@@ -67,35 +69,35 @@ export class State extends pulumi.ComponentResource {
     const connectionString = this.storeInVault(
       CONSTANTS.AzStorage_KV_CS_SecretName,
       `Storage--${CONSTANTS.AzStorage_KV_CS_SecretName}`,
-      this.storageAccount.primaryConnectionString,
+      this.storageAccount.primaryConnectionString
     );
     const accountKey = this.storeInVault(
       "StorageAccountKey",
-      'Storage--Key',
-      this.storageAccount.primaryAccessKey,
+      "Storage--Key",
+      this.storageAccount.primaryAccessKey
     );
     const gitHubTokenSecret = this.storeInVault(
       "gitHubToken",
       "GitHubOptions--Token",
-      pulumi.interpolate`${config.getSecret("gitHubToken")}`,
+      pulumi.interpolate`${config.getSecret("gitHubToken")}`
     );
     const appTopicPrimaryKey = this.storeInVault(
       "appTopicPrimaryKey",
       "EventGrid--AppTopic--PrimaryKey",
       this.monitoring.appTopic.primaryAccessKey,
-      this.monitoring.appTopic,
+      this.monitoring.appTopic
     );
     const appTopicSecondaryKey = this.storeInVault(
       "appTopicSecondaryKey",
       "EventGrid--AppTopic--SecondaryKey",
       this.monitoring.appTopic.secondaryAccessKey,
-      this.monitoring.appTopic,
+      this.monitoring.appTopic
     );
     const appTopicEndpoint = this.storeInVault(
       "appTopicEndpoint",
       "EventGrid--AppTopic--Endpoint",
       this.monitoring.appTopic.endpoint,
-      this.monitoring.appTopic,
+      this.monitoring.appTopic
     );
     this.createEventHubs(stateRg);
     this.createCosmosDb(stateRg);
@@ -115,7 +117,7 @@ export class State extends pulumi.ComponentResource {
       },
       {
         parent: rg,
-      },
+      }
     );
 
     return storage;
@@ -134,17 +136,62 @@ export class State extends pulumi.ComponentResource {
         tags,
         tenantId: CONSTANTS.authentication.tenantId,
       },
-      { parent: rg },
+      { parent: rg }
     );
 
     identities.forEach((element) => {
       if (element.appId) {
-        this.kvAccessPolicies.push(new azure.keyvault.AccessPolicy(
-          element.name + "app",
+        this.kvAccessPolicies.push(
+          new azure.keyvault.AccessPolicy(
+            element.name + "app",
+            {
+              applicationId: element.appId,
+              certificatePermissions: [
+                "get",
+                "create",
+                "list",
+                "update",
+                "delete",
+              ],
+              keyPermissions: [
+                "create",
+                "get",
+                "list",
+                "delete",
+                "unwrapKey",
+                "wrapKey",
+              ],
+              keyVaultId: kv.id,
+              objectId: element.objectId,
+              secretPermissions: ["list", "set", "get", "delete"],
+              tenantId: CONSTANTS.authentication.tenantId,
+            },
+            {
+              dependsOn: kv,
+              parent: this,
+            }
+          )
+        );
+      }
+      this.kvAccessPolicies.push(
+        new azure.keyvault.AccessPolicy(
+          element.name + "obj",
           {
-            applicationId: element.appId,
-            certificatePermissions: ["get", "create", "list", "update", "delete"],
-            keyPermissions: ["create", "get", "list", "delete", "unwrapKey", "wrapKey"],
+            certificatePermissions: [
+              "get",
+              "create",
+              "list",
+              "update",
+              "delete",
+            ],
+            keyPermissions: [
+              "create",
+              "get",
+              "list",
+              "delete",
+              "unwrapKey",
+              "wrapKey",
+            ],
             keyVaultId: kv.id,
             objectId: element.objectId,
             secretPermissions: ["list", "set", "get", "delete"],
@@ -153,27 +200,13 @@ export class State extends pulumi.ComponentResource {
           {
             dependsOn: kv,
             parent: this,
-          },
-        ));
-      }
-      this.kvAccessPolicies.push(new azure.keyvault.AccessPolicy(
-        element.name + "obj",
-        {
-          certificatePermissions: ["get", "create", "list", "update", "delete"],
-          keyPermissions: ["create", "get", "list", "delete", "unwrapKey", "wrapKey"],
-          keyVaultId: kv.id,
-          objectId: element.objectId,
-          secretPermissions: ["list", "set", "get", "delete"],
-          tenantId: CONSTANTS.authentication.tenantId,
-        },
-        {
-          dependsOn: kv,
-          parent: this,
-        },
-      ));
+          }
+        )
+      );
     });
 
-    const dataProtectionKey = new azure.keyvault.Key("dataprotectionkey",
+    const dataProtectionKey = new azure.keyvault.Key(
+      "dataprotectionkey",
       {
         keyOpts: [
           "decrypt",
@@ -191,9 +224,11 @@ export class State extends pulumi.ComponentResource {
       {
         dependsOn: this.kvAccessPolicies,
         parent: this,
-      });
+      }
+    );
 
-    const identitySigningCertificate = new azure.keyvault.Certificate("identityCert",
+    const identitySigningCertificate = new azure.keyvault.Certificate(
+      "identityCert",
       {
         certificatePolicy: {
           issuerParameters: {
@@ -204,7 +239,8 @@ export class State extends pulumi.ComponentResource {
             keySize: 2048,
             keyType: "RSA",
             reuseKey: true,
-          }, lifetimeActions: [
+          },
+          lifetimeActions: [
             {
               action: {
                 actionType: "AutoRenew",
@@ -229,48 +265,54 @@ export class State extends pulumi.ComponentResource {
             ],
             subject: `CN=${stack}-amphoradata`,
             subjectAlternativeNames: {
-              dnsNames: [
-                "localhost",
-                `${stack}.identity.amphoradata.com`,
-              ],
+              dnsNames: ["localhost", `${stack}.identity.amphoradata.com`],
             },
             validityInMonths: 12,
           },
         },
         keyVaultId: kv.id,
         name: "identityCert",
-      }, {
-      dependsOn: this.kvAccessPolicies,
-      parent: this,
-    });
+      },
+      {
+        dependsOn: this.kvAccessPolicies,
+        parent: this,
+      }
+    );
 
     // this.kvDiagnostics(kv, rg);
     return kv;
   }
 
-  private kvDiagnostics(kv: azure.keyvault.KeyVault, rg: azure.core.ResourceGroup) {
-    const kvDiagnostics = new azure.monitoring.DiagnosticSetting("keyVault-Diag", {
-      logAnalyticsWorkspaceId: this.monitoring.logAnalyticsWorkspace.id,
-      logs: [
-        {
-          category: "AuditEvent",
-          enabled: false,
-          retentionPolicy: {
+  private kvDiagnostics(
+    kv: azure.keyvault.KeyVault,
+    rg: azure.core.ResourceGroup
+  ) {
+    const kvDiagnostics = new azure.monitoring.DiagnosticSetting(
+      "keyVault-Diag",
+      {
+        logAnalyticsWorkspaceId: this.monitoring.logAnalyticsWorkspace.id,
+        logs: [
+          {
+            category: "AuditEvent",
             enabled: false,
+            retentionPolicy: {
+              enabled: false,
+            },
           },
-        },
-      ],
-      metrics: [
-        {
-          category: "AllMetrics",
-          retentionPolicy: {
-            enabled: false,
+        ],
+        metrics: [
+          {
+            category: "AllMetrics",
+            retentionPolicy: {
+              enabled: false,
+            },
           },
-        },
-      ],
-      name: "example",
-      targetResourceId: kv.id,
-    }, { parent: rg });
+        ],
+        name: "example",
+        targetResourceId: kv.id,
+      },
+      { parent: rg }
+    );
   }
 
   private createEventHubs(rg: azure.core.ResourceGroup) {
@@ -286,7 +328,7 @@ export class State extends pulumi.ComponentResource {
       },
       {
         parent: rg,
-      },
+      }
     );
 
     this.eh = new azure.eventhub.EventHub(
@@ -299,47 +341,55 @@ export class State extends pulumi.ComponentResource {
       },
       {
         parent: this.ehns,
-      },
+      }
     );
 
-    const ehAuthRule = new azure.eventhub.AuthorizationRule("ehAuthRule", {
-      eventhubName: this.eh.name,
-      listen: true,
-      manage: false,
-      namespaceName: this.ehns.name,
-      resourceGroupName: rg.name,
-      send: true,
-    }, {
-      parent: this.eh,
-    });
+    const ehAuthRule = new azure.eventhub.AuthorizationRule(
+      "ehAuthRule",
+      {
+        eventhubName: this.eh.name,
+        listen: true,
+        manage: false,
+        namespaceName: this.ehns.name,
+        resourceGroupName: rg.name,
+        send: true,
+      },
+      {
+        parent: this.eh,
+      }
+    );
 
-    this.storeInVault("TsiEventHubConnectionString", "TsiEventHub--ConnectionString",
-      ehAuthRule.primaryConnectionString);
+    this.storeInVault(
+      "TsiEventHubConnectionString",
+      "TsiEventHub--ConnectionString",
+      ehAuthRule.primaryConnectionString
+    );
     this.storeInVault("TsiEventHubName", "TsiEventHub--Name", this.eh.name);
-
   }
 
   private createCosmosDb(rg: azure.core.ResourceGroup) {
-    this.cosmosDb = new azure.cosmosdb.Account("cosmosDb", {
-      consistencyPolicy: {
-        consistencyLevel: "BoundedStaleness",
-        maxIntervalInSeconds: 10,
-        maxStalenessPrefix: 200,
-      },
-      enableAutomaticFailover: true,
-      geoLocations: [
-        {
-          failoverPriority: 0,
-          location: rg.location,
-        },
-      ],
-      kind: "GlobalDocumentDB",
-      location: rg.location,
-      offerType: "Standard",
-      resourceGroupName: rg.name,
-    },
+    this.cosmosDb = new azure.cosmosdb.Account(
+      "cosmosDb",
       {
-      });
+        consistencyPolicy: {
+          consistencyLevel: "BoundedStaleness",
+          maxIntervalInSeconds: 10,
+          maxStalenessPrefix: 200,
+        },
+        enableAutomaticFailover: true,
+        geoLocations: [
+          {
+            failoverPriority: 0,
+            location: rg.location,
+          },
+        ],
+        kind: "GlobalDocumentDB",
+        location: rg.location,
+        offerType: "Standard",
+        resourceGroupName: rg.name,
+      },
+      {}
+    );
 
     const dbNamePrefix = new random.RandomString(
       "db_name_prefix",
@@ -349,20 +399,24 @@ export class State extends pulumi.ComponentResource {
         number: false,
         special: false,
       },
-      { parent: this.cosmosDb },
+      { parent: this.cosmosDb }
     );
 
-    const dbA = new azure.cosmosdb.SqlDatabase("cosmosSql_A", {
-      accountName: this.cosmosDb.name,
-      // TODO: set this name as -A
-      resourceGroupName: rg.name,
-    },
+    const dbA = new azure.cosmosdb.SqlDatabase(
+      "cosmosSql_A",
+      {
+        accountName: this.cosmosDb.name,
+        // TODO: set this name as -A
+        resourceGroupName: rg.name,
+      },
       {
         aliases: [
           `urn:pulumi:${pulumi.getStack()}::${pulumi.getProject()}::azure:cosmosdb/sqlDatabase:SqlDatabase::cosmosSql`,
-          { name: "cosmosSql", parent: this.cosmosDb }],
+          { name: "cosmosSql", parent: this.cosmosDb },
+        ],
         parent: this.cosmosDb,
-      });
+      }
+    );
 
     // const dbB = new azure.cosmosdb.SqlDatabase("cosmosSql_B", {
     //   accountName: this.cosmosDb.name,
@@ -373,28 +427,50 @@ export class State extends pulumi.ComponentResource {
     //     parent: this.cosmosDb,
     //   });
 
-    this.storeInVault("cosmosAccountPrimaryKey", "Cosmos--PrimaryKey", this.cosmosDb.primaryMasterKey);
-    this.storeInVault("cosmosAccountSecondaryKey", "Cosmos--SecondaryKey", this.cosmosDb.secondaryMasterKey);
-    this.storeInVault("cosmosAccountPrimaryReadonlyKey", "Cosmos--PrimaryReadonlyKey",
-      this.cosmosDb.primaryReadonlyMasterKey);
-    this.storeInVault("cosmosAccountSecondaryReadonlyKey", "Cosmos--SecondaryReadonlyKey",
-      this.cosmosDb.secondaryReadonlyMasterKey);
+    this.storeInVault(
+      "cosmosAccountPrimaryKey",
+      "Cosmos--PrimaryKey",
+      this.cosmosDb.primaryMasterKey
+    );
+    this.storeInVault(
+      "cosmosAccountSecondaryKey",
+      "Cosmos--SecondaryKey",
+      this.cosmosDb.secondaryMasterKey
+    );
+    this.storeInVault(
+      "cosmosAccountPrimaryReadonlyKey",
+      "Cosmos--PrimaryReadonlyKey",
+      this.cosmosDb.primaryReadonlyMasterKey
+    );
+    this.storeInVault(
+      "cosmosAccountSecondaryReadonlyKey",
+      "Cosmos--SecondaryReadonlyKey",
+      this.cosmosDb.secondaryReadonlyMasterKey
+    );
 
-    this.storeInVault("cosmosAccountEndpoint", "Cosmos--Endpoint", this.cosmosDb.endpoint);
+    this.storeInVault(
+      "cosmosAccountEndpoint",
+      "Cosmos--Endpoint",
+      this.cosmosDb.endpoint
+    );
     this.storeInVault("cosmosSqlDbName", "Cosmos--Database", dbA.name);
 
     // TODO: use A/B databases
     this.storeInVault("cosmosSqlDbAName", "Cosmos--DatabaseA", dbA.name);
     // this.storeInVault("cosmosSqlDbBName", "Cosmos--DatabaseB", dbB.name);
 
-    this.storeInVault("SendGridApiKey", "SendGrid--ApiKey", config.require("SendGridApiKey"));
+    this.storeInVault(
+      "SendGridApiKey",
+      "SendGrid--ApiKey",
+      config.require("SendGridApiKey")
+    );
 
     // this.linkCosmosToLogAnalytics(this.cosmosDb, this.monitoring.logAnalyticsWorkspace);
   }
 
   private linkCosmosToLogAnalytics(
     cosmos: azure.cosmosdb.Account,
-    logAnalyticsWorkspace: azure.operationalinsights.AnalyticsWorkspace,
+    logAnalyticsWorkspace: azure.operationalinsights.AnalyticsWorkspace
   ) {
     const diags = new azure.monitoring.DiagnosticSetting(
       "cosmos-diag",
@@ -471,7 +547,7 @@ export class State extends pulumi.ComponentResource {
       },
       {
         parent: this,
-      },
+      }
     );
   }
 }
