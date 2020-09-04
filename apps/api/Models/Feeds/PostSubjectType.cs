a7db1c7372f69a087f237f10b9c7d2a176d1692d
Rian Finnegan <rian@amphoradata.com>
2020-09-04 12:11:04 +1000

    fix: #2183 use strings

--- apps/api/Models/Feeds/PostSubjectType.cs
+++ apps/api/Models/Feeds/PostSubjectType.cs
@@ -1,5 +1,9 @@
+using System.Text.Json.Serialization;
+using Newtonsoft.Json.Converters;
+
 namespace Amphora.Api.Models.Feeds
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum PostSubjectType
     {
         Amphora
