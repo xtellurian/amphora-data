aded94e1c2af6b3fdce6c032a22110eb23708a8f
Rian Finnegan <rian@amphoradata.com>
2020-09-08 16:02:54 +1000

    fix: #2201 terms content preview could be too big

--- apps/api/ClientApp/src/components/terms/TermsOfUseList.tsx
+++ apps/api/ClientApp/src/components/terms/TermsOfUseList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { TermsOfUseContext } from "react-amphora";
 import { TermsOfUse } from "amphoradata";
+import ReactMarkdown from "react-markdown";
 import { Link } from "react-router-dom";
 import { Row, Col } from "reactstrap";
 import { SecondaryButton, OverflowMenuButton } from "../molecules/buttons";
@@ -16,28 +17,42 @@ const TermsRowHeader: React.FC = () => {
                 <div className="txt-med">Name</div>
             </Col>
             <Col lg={5} xs={5} className="d-md-block d-none">
-                <div className="txt-med">Content</div>
+                <div className="txt-med">Content Preview</div>
             </Col>
             <Col xs={2} lg={2}></Col>
         </Row>
     );
 };
+const MAX_TERMS_LENGTH = 100;
 const TermsRow: React.FC<TermsListItemProps> = ({ terms }) => {
+    let contents = terms.contents;
+    if (contents.length > MAX_TERMS_LENGTH) {
+        const indexOfNewline = contents.indexOf("\n");
+        if (indexOfNewline > 0 && indexOfNewline < MAX_TERMS_LENGTH) {
+            contents = contents.substr(0, indexOfNewline);
+        } else {
+            contents = contents.substr(0, MAX_TERMS_LENGTH);
+        }
+    }
     return (
         <Row className="p-3 mt-1 align-items-center border rounded">
             <Col>{terms.name}</Col>
             <Col lg={5} xs={5} className="d-none d-md-block overflow-hidden">
-                {terms.contents}
+                <ReactMarkdown>{contents}</ReactMarkdown>
             </Col>
             <Col lg={2} className="d-none d-lg-block">
                 <Link to={`/terms/detail/${terms.id}`}>
-                    <SecondaryButton className="w-100 m-4">View</SecondaryButton>
+                    <SecondaryButton className="w-100 m-4">
+                        View
+                    </SecondaryButton>
                 </Link>
             </Col>
             <Col xs={2} className="d-lg-none d-block">
                 <OverflowMenuButton>
                     <Link to={`/terms/detail/${terms.id}`}>
-                        <SecondaryButton className="w-100">View</SecondaryButton>
+                        <SecondaryButton className="w-100">
+                            View
+                        </SecondaryButton>
                     </Link>
                 </OverflowMenuButton>
             </Col>
